3eb0b9996e44f95044a5c042cdf7e36236941afc
addshore <addshorewiki@gmail.com>
2019-08-17 15:25:05 -0600

    api, test user/self, but skip for now
    Need to figure out how to use actingAs correctly.
    This probably relates to AuthServiceProvider ?

--- src/tests/Routes/User/SelfTest.php
+++ src/tests/Routes/User/SelfTest.php
@@ -2,9 +2,11 @@
 
 namespace App\Tests\Routes\User;
 
+use App\User;
 use App\Tests\TestCase;
 use App\Tests\Routes\Traits\CrossSiteHeadersOnOptions;
 use App\Tests\Routes\Traits\OptionsRequestAllowed;
+use Laravel\Lumen\Testing\DatabaseTransactions;
 
 class SelfTest extends TestCase {
 
@@ -12,5 +14,16 @@ class SelfTest extends TestCase {
 
     use CrossSiteHeadersOnOptions;
     use OptionsRequestAllowed;
+
+    use DatabaseTransactions;
+
+    public function testGet() {
+        // TODO fix InvalidArgumentException: Auth driver [api] for guard [api] is not defined
+        $this->markTestSkipped();
+        $user = factory(User::class)->create();
+        $this->actingAs($user)
+          ->get($this->route)
+          ->seeStatusCode(200);
+    }
 
 }
