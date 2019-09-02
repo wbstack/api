62a5ff6eca935348499697018325745974d793ef
addshore <addshorewiki@gmail.com>
2019-09-02 10:23:39 -0500

    SHIT, remove TrustProxies for now

--- src/app/Http/Kernel.php
+++ src/app/Http/Kernel.php
@@ -18,7 +18,9 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-        \App\Http\Middleware\TrustProxies::class,
+        // SHIFT the below was giving Class 'Fideloper\Proxy\TrustProxies' not found
+        // and I don't know what it does yet, so juse remove it...
+        //\App\Http\Middleware\TrustProxies::class,
     ];
 
     /**
