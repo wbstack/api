ce091cbe9bc36cc4f82f007450e67654de3782a2
addshore <addshorewiki@gmail.com>
2019-09-02 11:57:17 -0500

    api, throttle login attempts :)

--- src/app/Http/Controllers/Auth/LoginController.php
+++ src/app/Http/Controllers/Auth/LoginController.php
@@ -7,24 +7,43 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Foundation\Auth\ThrottlesLogins;
 
 class LoginController extends Controller
 {
+    use ThrottlesLogins;
+
+    // Used by ThrottlesLogins
+    protected function username() {
+      return 'email';
+    }
+
     public function login(Request $request)
     {
+      // Validation
         $rules = [
           'email' => 'required|exists:users',
           'password'  => 'required',
       ];
         $request->validate($rules);
+
+        // Throttle
+        if ($this->hasTooManyLoginAttempts($request)) {
+            $this->fireLockoutEvent($request);
+            return $this->sendLockoutResponse($request);
+        }
+
+        // Try login
         $data = [
           'email' => $request->get('email'),
           'password'  =>  $request->get('password'),
           //'is_active' => true
       ];
         if (Auth::attempt($data)) {
+
+            $this->clearLoginAttempts($request);
+
             $user = Auth::user();
-            // the $user->createToken('appName')->accessToken generates the JWT token that we can use
             return response()->json([
               'user'  =>  $user, // <- we're sending the user info for frontend usage
               'token' =>  $user->createToken('yourAppName')->accessToken, // <- token is generated and sent back to the front end
@@ -33,6 +52,7 @@ class LoginController extends Controller
               'isAdmin' => $user->isAdmin(),
           ]);
         } else {
+            $this->incrementLoginAttempts($request);
             return response()->json('Unauthorized', 401);
         }
     }
