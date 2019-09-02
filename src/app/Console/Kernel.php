5d326b49527bbeefe93b6d30186493036e0a75a4
addshore <addshorewiki@gmail.com>
2019-09-02 10:24:22 -0500

    SHIFT, Console Kernel fixes

--- src/app/Console/Kernel.php
+++ src/app/Console/Kernel.php
@@ -4,6 +4,8 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use App\Jobs\EnsureWikiDbPoolPopulatedJob;
+use App\Jobs\ExpireOldUserVerificationTokensJob;
 
 class Kernel extends ConsoleKernel
 {
---
+++ src/routes/console.php
@@ -0,0 +1,4 @@
+<?php
+
+// SHIFT NOTE: nothing in here, but laravel seemed to want it..
+// This is loaded by the Console Kernel..
