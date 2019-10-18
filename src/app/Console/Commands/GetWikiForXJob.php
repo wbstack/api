43c71854673530c2626f2ace2753fa9344a0988b
addshore <addshorewiki@gmail.com>
2019-10-18 21:52:36 +0100

    api, cli command for getting wiki

---
+++ src/app/Console/Commands/GetWikiForXJob.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Wiki;
+use Illuminate\Console\Command;
+
+class GetWikiForXJob extends Command
+{
+    protected $signature = 'app:getWiki {key} {value?}';
+
+    protected $description = 'Get Wiki For X job';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $key = trim($this->argument('key'));
+        $value = trim($this->argument('value'));
+        $result = Wiki::where($key, $value)->with(['wikiDb','wikiQueryserviceNamespace'])->first();
+
+        $this->info(json_encode($result));
+    }
+}
