f22ce291c52a7f535f2ac9b59aa3d407a72554fd
addshore <addshorewiki@gmail.com>
2019-10-21 20:03:47 +0200

    localhost dev, allow testing multiple domains...
    *.localhost can be used now where * is the domain in the actual
    system

--- src/app/Http/Controllers/Backend/WikiController.php
+++ src/app/Http/Controllers/Backend/WikiController.php
@@ -8,17 +8,24 @@ use Illuminate\Http\Request;
 
 class WikiController extends Controller
 {
+    private static $with = ['wikiDb','wikiQueryserviceNamespace','settings'];
+
     public function getWikiForDomain(Request $request)
     {
         $domain = $request->input('domain');
 
-        // If we are developing and the request is localhost or mediawiki then return the settings of the first wiki :)
-        if ($domain === 'localhost' || $domain === 'mediawiki') {
-            // TODO if not in debug mode don't allow this code path to run
-            $result = Wiki::with(['wikiDb','wikiQueryserviceNamespace','settings'])->first();
+        // TODO don't do the localhost checks when in production? :)
+        if ( substr($domain,-10, 10) === '.localhost' ){
+            // localhost development, with a full domain prefixing .localhost
+            // eg. wiki.addshore.com.localhost
+            $result = Wiki::where('domain', substr($domain,0, -10))->with(self::$with)->first();
+        } else if ( $domain === 'localhost' || $domain === 'mediawiki' ) {
+            // If just using localhost then just get the first undeleted wiki
+            // TODO actually check this doesn't show deleted wikis?
+            $result = Wiki::with(self::$with)->first();
         } else {
             // TODO don't select the timestamps and redundant info for the settings?
-            $result = Wiki::where('domain', $domain)->with(['wikiDb','wikiQueryserviceNamespace','settings'])->first();
+            $result = Wiki::where('domain', $domain)->with(self::$with)->first();
         }
 
         $res['success'] = true;
