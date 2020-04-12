513d03269b8c91110438b120d76052d46e4f2acf
addshore <adamshorland@gmail.com>
2020-04-12 16:29:18 +0100

    Reduce mediawiki layers, some tweaks and investigate #41
    Regarding #41 right now the code that gets the wiki settings
    has 0 idea about when the internal API ERRORS or when it returns null
    data.
    That distinction probably needs to be made and some sort of different
    message passed down to the user instead of the current behaviour..

--- src/app/Http/Controllers/Backend/WikiController.php
+++ src/app/Http/Controllers/Backend/WikiController.php
@@ -23,14 +23,12 @@ class WikiController extends Controller
             $result = Wiki::where('domain', substr($domain, 0, -10))->with(self::$with)->first();
         } elseif ($domain === 'localhost' || $domain === 'mediawiki') {
             // If just using localhost then just get the first undeleted wiki
-            // TODO actually check this doesn't show deleted wikis?
             $result = Wiki::with(self::$with)->first();
         } else {
             // TODO don't select the timestamps and redundant info for the settings?
             $result = Wiki::where('domain', $domain)->with(self::$with)->first();
         }
 
-        $res['success'] = true;
         $res['data'] = $result;
 
         return response($res);
