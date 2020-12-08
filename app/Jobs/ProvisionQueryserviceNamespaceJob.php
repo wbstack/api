wbstack/api

commit c259db095641660c27e5c8ef1a5c7a528e9b4a3e

Rework storage pool provisioning jobs
Instead of having 3 jobs this now just has 2.
Each job takes care of if the state of the pool, and if
we should need to create more.

This also means that at most 1 new entry will be created per
min. Which is fine for now, but might change in the future if
lots of things get created at once.

app/Jobs/ProvisionQueryserviceNamespaceJob.php
@@ -11,11 +11,12 @@ use App\QueryserviceNamespace;
 class ProvisionQueryserviceNamespaceJob extends Job
 {
     private $namespace;
+    private $maxFree;
 
     /**
      * @return void
      */
-    public function __construct($namespace = null)
+    public function __construct($namespace = null, $maxFree = null)
     {
         if ($namespace !== null && preg_match('/[^A-Za-z0-9]/', $namespace)) {
             throw new \InvalidArgumentException('$namespace must only contain [^A-Za-z0-9] or null, got '.$namespace);
@@ -28,6 +29,14 @@ class ProvisionQueryserviceNamespaceJob extends Job
         }
 
         $this->namespace = $namespace;
+        $this->maxFree = $maxFree;
+    }
+
+    private function doesMaxFreeSayWeShouldStop(){
+        $wikiDbCondition = ['wiki_id' => null,'version' => $this->newSqlFile];
+        $unassignedDbs = WikiDb::where($wikiDbCondition)->count();
+        $toCreate = $this->maxFree - $unassignedDbs;
+        return $toCreate === 0;
     }
 
     /**
@@ -35,6 +44,11 @@ class ProvisionQueryserviceNamespaceJob extends Job
      */
     public function handle()
     {
+        // If the job is only meant to create so many DBs, then make sure we don't create too many.
+        if( $this->maxFree && $this->doesMaxFreeSayWeShouldStop() ){
+            return;
+        }
+
         $properties = file_get_contents(__DIR__.DIRECTORY_SEPARATOR.'../data/RWStore.properties');
         // Currently only one, but will change at some point...
         $queryServiceHost = config('app.queryservice_host');

app/Console/Kernel.php
@@ -5,7 +5,8 @@ namespace App\Console;
 use App\Jobs\PruneEventPageUpdatesTable;
 use App\Jobs\PruneQueryserviceBatchesTable;
 use Illuminate\Console\Scheduling\Schedule;
-use App\Jobs\EnsureStoragePoolsPopulatedJob;
+use App\Jobs\ProvisionWikiDbJob;
+use App\Jobs\ProvisionQueryserviceNamespaceJob;
 use App\Jobs\ExpireOldUserVerificationTokensJob;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -20,7 +21,13 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // TODO it would be good if some of these could be opportunist & triggered rather than crons..
-        $schedule->job(new EnsureStoragePoolsPopulatedJob)->everyMinute();
+
+        // Make sure that the DB and QS pools are always populated somewhat.
+        // This will create at most 1 new entry for each per minute...
+        $schedule->job(new ProvisionWikiDbJob(null,null,10))->everyMinute();
+        $schedule->job(new ProvisionQueryserviceNamespaceJob(null,10))->everyMinute();
+
+        // Slowly cleanup some tables
         $schedule->job(new ExpireOldUserVerificationTokensJob)->hourly();
         $schedule->job(new PruneEventPageUpdatesTable)->everyFifteenMinutes();
         $schedule->job(new PruneQueryserviceBatchesTable)->everyFifteenMinutes();

app/Jobs/EnsureStoragePoolsPopulatedJob.php
@@ -1,48 +1,0 @@
-<?php
-
-namespace App\Jobs;
-
-use App\WikiDb;
-use App\QueryserviceNamespace;
-
-/**
- * This job runs on a cron and ensures the pools are filled that are needed for fast wiki creation
- *
- * Example usage:
- * php artisan wbs-job:handle EnsureStoragePoolsPopulatedJob
- */
-class EnsureStoragePoolsPopulatedJob extends Job
-{
-    private $requiredDbs = 10;
-    private $requiredQueryServiceNS = 10;
-
-    /**
-     * @return void
-     */
-    public function handle()
-    {
-        // TODO these these bits should enter rows that say PENDING?!
-        // then if rows say pending trigger a job to create them? :)
-        // This avoid these jobs running away and creating things if they then fail to insert records into the sql..
-
-        // Wiki Dbs
-        $wikiDbCondition = ['wiki_id'=>null,'version'=>'mw1.35-wbs1'];
-        $unassignedDbs = WikiDb::where($wikiDbCondition)->count();
-        $toCreate = $this->requiredDbs - $unassignedDbs;
-        if ($toCreate > 0) {
-            for ($i = 0; $i < $toCreate; $i++) {
-                dispatch(new ProvisionWikiDbJob());
-            }
-        }
-
-        // Query service namespaces
-        $unassignedQueryserviceNamespaces = QueryserviceNamespace::where('wiki_id', null)->count();
-        $toCreate = $this->requiredQueryServiceNS - $unassignedQueryserviceNamespaces;
-        if ($toCreate > 0) {
-            for ($i = 0; $i < $toCreate; $i++) {
-                dispatch(new ProvisionQueryserviceNamespaceJob());
-            }
-        }
-    }
-
-}

app/Jobs/ProvisionWikiDbJob.php
@@ -6,7 +6,7 @@ use App\WikiDb;
 use Illuminate\Support\Facades\DB;
 
 /**
- * Example usage
+ * Example usage that will always provision a new DB:
  * php artisan wbs-job:handle ProvisionWikiDbJob
  */
 class ProvisionWikiDbJob extends Job
@@ -30,10 +30,12 @@ class ProvisionWikiDbJob extends Job
 
     private $dbPassword;
 
+    private $maxFree;
+
     /**
      * @return void
      */
-    public function __construct($prefix = null, $dbName = false)
+    public function __construct($prefix = null, $dbName = false, $maxFree = null)
     {
         if (preg_match('/[^A-Za-z0-9\-_]/', $prefix)) {
             throw new \InvalidArgumentException('Prefix must only contain [^A-Za-z0-9\-_], got '.$prefix);
@@ -60,6 +62,14 @@ class ProvisionWikiDbJob extends Job
 
         $this->prefix = $prefix;
         $this->dbName = $dbName;
+        $this->maxFree = $maxFree;
+    }
+
+    private function doesMaxFreeSayWeShouldStop(){
+        $wikiDbCondition = ['wiki_id' => null,'version' => $this->newSqlFile];
+        $unassignedDbs = WikiDb::where($wikiDbCondition)->count();
+        $toCreate = $this->maxFree - $unassignedDbs;
+        return $toCreate === 0;
     }
 
     /**
@@ -67,6 +77,11 @@ class ProvisionWikiDbJob extends Job
      */
     public function handle()
     {
+        // If the job is only meant to create so many DBs, then make sure we don't create too many.
+        if( $this->maxFree && $this->doesMaxFreeSayWeShouldStop() ){
+            return;
+        }
+
         $conn = DB::connection($this->dbConnection);
         $pdo = $conn->getPdo();
 
@@ -141,7 +156,7 @@ class ProvisionWikiDbJob extends Job
         //  - update the wikiDb Record on success
         //
         //  This way, if this wikiDb create fails, there is still a record of the DB that has been / is being created?
-        $wikiDb = WikiDb::create([
+        WikiDb::create([
           'name' => $this->dbName,
           'user' => $this->dbUser,
           'password' => $this->dbPassword,

commit 1a4c08b759b76372f997cdda6dcf350af387d05e

Fix QueryserviceNamespace provisioning
Broken recently in c259db095641660c27e5c8ef1a5c7a528e9b4a3e

app/Jobs/ProvisionQueryserviceNamespaceJob.php
@@ -33,9 +33,8 @@ class ProvisionQueryserviceNamespaceJob extends Job
     }
 
     private function doesMaxFreeSayWeShouldStop(){
-        $wikiDbCondition = ['wiki_id' => null,'version' => $this->newSqlFile];
-        $unassignedDbs = WikiDb::where($wikiDbCondition)->count();
-        $toCreate = $this->maxFree - $unassignedDbs;
+        $unassignedQueryserviceNamespaces = QueryserviceNamespace::where('wiki_id', null)->count();
+        $toCreate = $this->maxFree - $unassignedQueryserviceNamespaces;
         return $toCreate === 0;
     }
 