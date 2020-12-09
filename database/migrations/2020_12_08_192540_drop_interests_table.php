ff6f2b1ef404fe3e34e0a45543caff92e0100836
Jakob Warkotsch <jakob.warkotsch@wikimedia.de>
2020-12-09 12:44:24 +0100

    Fix migration script class name

--- database/migrations/2020_12_08_192540_drop_interests_table.php
+++ database/migrations/2020_12_08_192540_drop_interests_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateInterestsTable extends Migration{
+class DropInterestsTable extends Migration{
     public function up(){
         Schema::dropIfExists('interests');
     }
