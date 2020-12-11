8c3d6b29bd4711972da7f7c0207fc263421e1a54
rosalie <perside.rosalie@wikimedia.de>
2020-12-11 15:10:48 +0100

    Add sandbox cleanup job

---
+++ app/Jobs/SandboxCleanupJob.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Wiki;
+use App\WikiSetting;
+
+class SandboxCleanupJob extends Job {
+
+    public function handle(){
+        Wiki::whereIn( 'id', WikiSetting::whereName('wwSandboxAutoUserLogin')->pluck('wiki_id')->toArray())
+            ->where('created_at', '<', date('Y-m-d', strtotime('-1 week')))->delete();
+    }
+
+}
