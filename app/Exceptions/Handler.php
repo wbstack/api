8f0ddb6e25bbf1da4e5edd439c7823976cb922c1
addshore <addshorewiki@gmail.com>
2021-03-26 20:13:54 +0000

    Throwable for App\Exceptions\Handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -28,24 +28,24 @@ class Handler extends ExceptionHandler
     /**
      * Report or log an exception.
      *
-     * @param  \Exception  $exception
+     * @param  \Throwable  $throwable
      * @return void
      */
-    public function report(Exception $exception)
+    public function report(Throwable $throwable)
     {
-        StackdriverExceptionHandler::report($exception);
-        parent::report($exception);
+        StackdriverExceptionHandler::report($throwable);
+        parent::report($throwable);
     }
 
     /**
      * Render an exception into an HTTP response.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \Exception  $exception
+     * @param  \Throwable  $throwable
      * @return \Illuminate\Http\Response
      */
-    public function render($request, Exception $exception)
+    public function render($request, Throwable $throwable)
     {
-        return parent::render($request, $exception);
+        return parent::render($request, $throwable);
     }
 }
