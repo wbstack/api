d79a7aa7f8a0d6e9fe9cd7c6c39fe369f1a1eb05
Bereket teshome <bereket.teshome@wikimedia.de>
2021-07-05 14:24:11 +0200

    Added job to create elastic indexes when a new wiki is created
    Bug: T285541

--- app/Http/Controllers/WikiController.php
+++ app/Http/Controllers/WikiController.php
@@ -79,6 +79,14 @@ class WikiController extends Controller
                 'value' => Str::random(64),
             ]);
 
+            // Enable elasticsearch for new wikis by default
+            // T285541
+            WikiSetting::create([
+                'wiki_id' => $wiki->id,
+                'name' => 'wwExtEnableElasticSearch',
+                'value' => true,
+            ]);
+
             // Also track the domain forever in your domains table
             $wikiDomain = WikiDomain::create([
                 'domain' => $wiki->domain,
@@ -107,6 +115,8 @@ class WikiController extends Controller
         // opportunistic dispatching of jobs to make sure storage pools are topped up
         $this->dispatch(new ProvisionWikiDbJob(null, null, 10));
         $this->dispatch(new ProvisionQueryserviceNamespaceJob(null, 10));
+
+        $this->dispatch(new ElasticSearchIndexInit($wiki->domain));
 
         $res['success'] = true;
         $res['message'] = 'Success!';
