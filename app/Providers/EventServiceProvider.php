0383f1cfaa2fef67892f1449ab4d8ab8619d533e
toan <tobias.andersson@wikimedia.de>
2021-08-11 09:39:29 +0200

    Add ElasticSearchWikiSettingsObserver
    This adds an observer to the changes coming into all settings. It's
    currently looking only for updated/created events on the lexeme or
    elasticsearch setting and triggers the index update job if they are both
    enabled at the same time. This change also makes the update job unique
    to avoid duplicates.
    
    I thought this could be a nice way to manage the lifecycle of these
    settings in separate classes rather than stuffing everything into the
    update controller.

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -5,6 +5,8 @@ namespace App\Providers;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+use App\WikiSetting;
+use App\Observers\ElasticSearchWikiSettingsObserver;
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -27,7 +29,6 @@ class EventServiceProvider extends ServiceProvider
     public function boot()
     {
         parent::boot();
-
-        //
+        WikiSetting::observe(ElasticSearchWikiSettingsObserver::class);
     }
 }
