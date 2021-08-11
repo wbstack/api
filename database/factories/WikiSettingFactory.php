df60dc840e9423072f62ce17a4f06ff5e8b9053c
toan <tobias.andersson@wikimedia.de>
2021-08-11 09:37:39 +0200

    Add WikiSetting factory

---
+++ database/factories/WikiSettingFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\WikiSetting;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class WikiSettingFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = WikiSetting::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [];
+    }
+}
