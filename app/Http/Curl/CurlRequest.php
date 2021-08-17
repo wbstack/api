b7889196564662fe9026e9dd2ffe0fe42b6a6211
toan <tobias.andersson@wikimedia.de>
2021-08-17 10:10:10 +0200

    Add CurlRequest + HttpRequest inteface
    Abstract curl requests to an interface for DI

---
+++ app/Http/Curl/CurlRequest.php
@@ -0,0 +1,40 @@
+<?php
+namespace App\Http\Curl;
+
+class CurlRequest implements HttpRequest
+{
+    private $handle = null;
+
+    public function __construct() {
+        $this->reset();
+    }
+
+    public function setOptions( array $options ) {
+        curl_setopt_array( $this->handle, $options );
+    }
+
+    public function execute() {
+        return curl_exec($this->handle);
+    }
+
+    public function error() {
+        return curl_error($this->handle);
+    }
+
+    public function getInfo($name) {
+        return curl_getinfo($this->handle, $name);
+    }
+
+    public function close() {
+        curl_close($this->handle);
+        $this->handle = null;
+    }
+
+    public function reset() {
+        if( $this->handle ) {
+            $this->close();
+        }
+
+        $this->handle = curl_init();
+    }
+}
