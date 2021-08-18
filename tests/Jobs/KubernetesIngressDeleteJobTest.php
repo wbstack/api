e6348cf8ce7ae6143301c069517973f30d710041
toan <tobias.andersson@wikimedia.de>
2021-08-18 10:12:52 +0200

    Add K8s ingress deletion job
    This turned out to be quite the blackbox, tried setting up minikube for
    testing but wouldn't work without somekind of proxy thing to make them
    talk between docker <-> minikube which feels to be a step too far. Do we
    just trust this api to work?

---
+++ tests/Jobs/KubernetesIngressDeleteJobTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Tests\Jobs;
+
+use App\User;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Tests\TestCase;
+use App\Jobs\KubernetesIngressDeleteJob;
+use App\Wiki;
+use App\WikiManager;
+use Illuminate\Contracts\Queue\Job;
+
+class KubernetesIngressDeleteJobTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    public function testDoesNotDeleteNonDeletedWikiws()
+    {
+     
+        $user = User::factory()->create(['verified' => true]);
+        $wiki = Wiki::factory()->create( [ 'deleted_at' => null ] );
+        WikiManager::factory()->create(['wiki_id' => $wiki->id, 'user_id' => $user->id]);
+        
+        $mockJob = $this->createMock(Job::class);
+        $mockJob->expects($this->once())
+            ->method('fail')
+            ->with(new \RuntimeException("Wiki 1 is not deleted, but it's ingress got called to be deleted."));
+
+        $job = new KubernetesIngressDeleteJob(1);
+        $job->setJob($mockJob);
+        
+        $job->handle();
+
+
+
+    }
+
+}
