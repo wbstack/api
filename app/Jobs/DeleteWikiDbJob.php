c9af00827286b3358ed053f68c43f57764dc264f
Tobias <tobias.ande@gmail.com>
2021-09-24 10:07:21 +0200

    [STANDALONE] Delete wiki db (#157)
    * Add DeleteWikiDbJob
    
    Adds a job that deletes the database and the user of a wiki.
    
    * Update app/Jobs/DeleteWikiDbJob.php
    
    Co-authored-by: Lucas Werkmeister <mail@lucaswerkmeister.de>
    
    * Update app/Jobs/DeleteWikiDbJob.php
    
    Co-authored-by: Lucas Werkmeister <mail@lucaswerkmeister.de>
    
    * Update failure message
    
    Co-authored-by: Lucas Werkmeister <mail@lucaswerkmeister.de>
    
    * Rename user + database instead of dropping anything
    
    This renames the database and user instead of dropping anything and adds
    the `deleted_` prefix infront of the names. Adds some test for the
    failures.
    
    * Fix review comments.
    
    - rename database with timestamp
    - rename tables without prefix
    - drop the user
    - remove foreign key conditional
    - some changes to how the tests use exec / query and do cleanup
    
    * Stop using doctrine abstractions
    
    - close connections after jobs
    - passes DatabaseManager into
     DeleteWikiDbJob and ProvisionWikiDbJob to make sure we don't reuse the
     connection.
    
    Co-authored-by: Lucas Werkmeister <mail@lucaswerkmeister.de>

---
+++ app/Jobs/DeleteWikiDbJob.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace App\Jobs;
+
+use App\WikiDb;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use App\Wiki;
+use Carbon\Carbon;
+use Illuminate\Database\DatabaseManager;
+
+/**
+ * Prepends the MW Database, User with `deleted_` prefix and deletes WikiDB relation for a wiki 
+ */
+class DeleteWikiDbJob extends Job implements ShouldBeUnique
+{
+    private $wikiId;
+
+    /**
+     * @return void
+     */
+    public function __construct( int $wikiId )
+    {
+        $this->wikiId = $wikiId;
+    }
+
+    /**
+     * The unique ID of the job.
+     *
+     * @return string
+     */
+    public function uniqueId()
+    {
+        return strval($this->wikiId);
+    }
+
+    /**
+     * @return void
+     */
+    public function handle( DatabaseManager $manager )
+    {
+        $wiki = Wiki::withTrashed()->where(['id' => $this->wikiId])->first();
+
+        if( !$wiki ) {
+            $this->fail(new \RuntimeException("Wiki not found for {$this->wikiId}"));
+            return;
+        }
+
+        if( !$wiki->deleted_at ) {
+            $this->fail(new \RuntimeException("Wiki {$this->wikiId} is not marked for deletion."));
+            return;
+        }
+
+        $wikiDB = WikiDb::whereWikiId( $this->wikiId )->first();
+
+        if( !$wikiDB ) {
+            $this->fail(new \RuntimeException("WikiDb not found for {$this->wikiId}"));
+            return;
+        }
+
+        try {
+
+            $manager->purge('mw');
+            $conn = $manager->connection('mw');
+
+            if (! $conn instanceof \Illuminate\Database\Connection) {
+               throw new \RuntimeException('Must be run on a PDO based DB connection');
+            }
+    
+            $pdo = $conn->getPdo();
+            $timestamp = Carbon::now()->timestamp;
+            $deletedDatabaseName = "deleted_{$timestamp}_{$this->wikiId}";
+    
+            if ($pdo->exec('USE '.$wikiDB->name) === false) {
+                throw new \RuntimeException('Failed to use database with dbname: '.$wikiDB->name);
+            }
+    
+
+            $tables = [];
+            $result = $pdo->query('SHOW TABLES')->fetchAll();
+            /*
+            ^ array:2 [
+            "Tables_in_<database_name>" => "<table_name>"
+            0 => "<table_name>"
+            ]
+            */
+            foreach($result as $table) {
+                $values = array_unique(array_values($table));
+                if(count($values) !== 1) {
+                    throw new \RuntimeException("Tried getting table names for wikiDB {$wikiDB->name} but failed");
+                }
+    
+                $tables[] = $values[0];
+            }
+
+            if( empty($tables) ) {
+                throw new \RuntimeException("Tried getting table names for wikiDB {$wikiDB->name} but did not find any");
+            }
+
+            $pdo->beginTransaction();
+
+            // Create the new database
+            $pdo->exec(sprintf('CREATE DATABASE %s', $deletedDatabaseName));
+
+            // iterate over each table and replace the prefix
+            // and move it to the deleted database by using RENAME TABLE
+            foreach($tables as $table) {
+
+                $replacedCount = 0;
+                $tableWithoutPrefix = str_replace($wikiDB->prefix . '_', '', $table, $replacedCount );
+                if ($replacedCount !== 1) {
+                    throw new \RuntimeException("Did not find prefix '{$wikiDB->prefix}' in tablename '{$table}' ");
+                }
+                $pdo->exec(sprintf('RENAME TABLE %s.%s TO %s.%s', $wikiDB->name, $table, $deletedDatabaseName, $tableWithoutPrefix));
+            }
+
+            $pdo->exec(sprintf('DROP USER %s', $wikiDB->user ));
+
+            $pdo->commit();
+        } catch (\Throwable $e) {
+            
+            $pdo->rollBack();
+
+            $manager->purge('mw');
+            $this->fail( new \RuntimeException('Failed to soft-soft delete '.$wikiDB->name . ': ' . $e->getMessage()) );
+            return;
+            
+        }
+
+        $wikiDB->delete();
+        $manager->purge('mw');
+    }
+}
---
+++ tests/Jobs/DeleteWikiJobTest.php
@@ -0,0 +1,193 @@
+<?php
+
+namespace Tests\Jobs;
+
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Tests\TestCase;
+use App\Jobs\DeleteWikiDbJob;
+use App\User;
+use App\Wiki;
+use App\WikiManager;
+use App\WikiDb;
+use App\Jobs\ProvisionWikiDbJob;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Contracts\Queue\Job;
+use Carbon\Carbon;
+use PDOException;
+use PHPUnit\TextUI\RuntimeException;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Database\DatabaseManager;
+
+class DeleteWikiJobTest extends TestCase
+{
+    use DatabaseTransactions;
+    use DispatchesJobs;
+
+    private $wiki;
+
+    protected function setUp(): void {
+        parent::setUp();
+        DB::connection('mysql')->getPdo()->exec('DROP DATABASE IF EXISTS the_test_database; DROP DATABASE IF EXISTS the_test_database_not_to_be_deleted');
+    }
+
+    private function getExpectedDeletedDatabaseName( $wiki ): string {
+        return "deleted_1631534400_" . $wiki->id;
+    }
+
+    private function getResultValues( $resultRows ) {
+        $results = [];
+        foreach($resultRows as $row) {
+            $results[] = array_unique(array_values($row))[0];
+        }
+
+        return $results;
+    }
+
+    public function testDispatching() {
+        $mockJob = $this->createMock(Job::class);
+        $job = new DeleteWikiDbJob(-1);
+        $job->setJob($mockJob);
+        $mockJob->expects($this->once())
+            ->method('fail');
+        $this->dispatchNow($job);
+    }
+
+    public function testDeletesWiki()
+    {
+        Carbon::setTestNow(Carbon::create(2021, 9, 13, 12));
+
+        $user = User::factory()->create(['verified' => true]);
+        $this->wiki = Wiki::factory()->create( [ 'deleted_at' => Carbon::now()->timestamp ] );
+        WikiManager::factory()->create(['wiki_id' => $this->wiki->id, 'user_id' => $user->id]);
+
+        $databaseName = 'the_test_database';
+        $expectedDeletedName = $this->getExpectedDeletedDatabaseName( $this->wiki );
+        $databases = [
+            [
+                "prefix" => "prefix",
+                "name" => "the_test_database"
+            ],
+            [
+                "prefix" => "prefix2",
+                "name" => "the_test_database_not_to_be_deleted"
+            ]
+        ];
+
+        // Would be injected by the app
+        $manager = $this->app->make('db');
+  
+        $job = new ProvisionWikiDbJob($databases[0]['prefix'], $databases[0]['name'], null);
+        $job->handle($manager);
+
+        // Would be injected by the app
+        $manager = $this->app->make('db');
+  
+        $job = new ProvisionWikiDbJob($databases[1]['prefix'], $databases[1]['name'], null);
+        $job->handle($manager);
+
+        $databaseName = 'the_test_database';
+
+        // set the first database to be this wikis database
+        WikiDb::where([
+            'name' => $databaseName,
+        ])->first()->update(['wiki_id' => $this->wiki->id]);
+
+        // make sure it stuck
+        $wikiDB = WikiDb::where([ 'wiki_id' => $this->wiki->id ])->first();
+        $this->assertNotNull( $wikiDB );
+
+        // get a new connection and look at the tables for later assertions
+        $conn = $this->app->make('db')->connection('mw');
+        $pdo = $conn->getPdo();
+        $pdo->exec("USE {$wikiDB->name}");
+        $initialTables = $pdo->query("SHOW TABLES")->fetchAll();
+        $conn->disconnect();
+
+        // we now have some mediawiki tables here
+        $this->assertCount(85, $initialTables);
+
+        $mockJob = $this->createMock(Job::class);
+        $mockJob->expects($this->never())->method('fail');
+
+        // would get injected by the app
+        $manager = $this->app->make('db');
+
+        // this job will kill the underlying connection
+        $job = new DeleteWikiDbJob( $this->wiki->id );
+        $job->setJob($mockJob);
+        $job->handle($manager);
+
+        // get a new connection and take a look at the database tables and newly created databases
+        $conn = $this->app->make('db')->connection('mw');
+        $pdo = $conn->getPdo();
+        $pdo->exec("USE {$wikiDB->name}");
+        $databases = $this->getResultValues($pdo->query("SHOW DATABASES")->fetchAll());
+
+        $this->assertNull( WikiDb::where([ 'wiki_id' => $this->wiki->id ])->first() );
+
+        // Both databases now exist, nothing has been dropped
+        $this->assertContains( $expectedDeletedName, $databases);
+
+        // after delete job we don't have any tables here any more
+        $this->assertCount(0, $this->getResultValues( $pdo->query("SHOW TABLES")->fetchAll()));
+
+        // all tables are now in the new deleted database
+        $pdo->exec("USE {$expectedDeletedName}");
+        $this->assertCount(85, $this->getResultValues( $pdo->query("SHOW TABLES")->fetchAll()));
+
+        // Content now live in the deleted database
+        $result = $pdo->query(sprintf('SELECT * FROM %s.interwiki', $expectedDeletedName))->fetchAll();
+        $this->assertCount(66, $result);
+
+        // cleanup test deleted database
+        $pdo->exec("DROP DATABASE {$this->getExpectedDeletedDatabaseName( $this->wiki )}");
+
+        // Tables no longer exist in the old one
+        $this->expectException(PDOException::class);
+        $this->expectExceptionMessage("SQLSTATE[42S02]: Base table or view not found: 1146 Table 'the_test_database.prefix_interwiki' doesn't exist");
+        $pdo->exec(sprintf('SELECT * FROM %s.%s_interwiki', $databaseName, $wikiDB->prefix ));
+    }
+
+    /**
+	 * @dataProvider failureProvider
+	 */
+    public function testFailure( $wiki_id, $deleted_at, string $expectedFailure)
+    {
+        if ($wiki_id !== -1) {
+            $wiki = Wiki::factory()->create( [  'deleted_at' => $deleted_at ] );
+            $wiki_id = $wiki->id;
+        }
+
+        $mockMananger = $this->createMock(DatabaseManager::class);
+
+        $mockJob = $this->createMock(Job::class);
+        $mockJob->expects($this->once())
+                ->method('fail')
+                ->with(new \RuntimeException(str_replace('<WIKI_ID>', $wiki_id, $expectedFailure)));
+                
+        $job = new DeleteWikiDbJob($wiki_id);
+        $job->setJob($mockJob);
+        $job->handle($mockMananger);
+    }
+
+    public function failureProvider() {
+
+        yield [
+            -1,
+            null,
+            'Wiki not found for <WIKI_ID>',
+        ];
+
+        yield [
+            1,
+            null,
+            'Wiki <WIKI_ID> is not marked for deletion.',
+        ];
+
+        yield [
+            1,
+            Carbon::now()->subDays(30)->timestamp,
+            'WikiDb not found for <WIKI_ID>',
+        ];
+    }
+}
