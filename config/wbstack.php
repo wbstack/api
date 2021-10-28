276a18d30a14fbbb635358165a2ee67eac7614ef
addshore <addshorewiki@gmail.com>
2021-10-28 15:56:09 +0100

    Also make used db version configurable

--- config/wbstack.php
+++ config/wbstack.php
@@ -7,5 +7,6 @@ return [
     'ui_url' => env('WBSTACK_UI_URL', 'https://wbstack.com'),
 
     'wiki_db_provision_version' => env('WBSTACK_WIKI_DB_PROVISION_VERSION', 'mw1.36-wbs1'),
+    'wiki_db_use_version' => env('WBSTACK_WIKI_DB_USE_VERSION', 'mw1.36-wbs1'),
 
 ];
