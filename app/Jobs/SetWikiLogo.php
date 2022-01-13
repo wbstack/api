2fa365536e548158877ef0c2f54aafbdbdcdf6e3
Ollie Shotton <oliver.shotton@wikimedia.de>
2022-01-13 10:20:06 +0000

    Upload and resize wiki logo

--- app/Jobs/SetWikiLogo.php
+++ app/Jobs/SetWikiLogo.php
@@ -3,6 +3,11 @@
 namespace App\Jobs;
 
 use App\Wiki;
+use Illuminate\Contracts\Filesystem\Cloud;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Http\File;
+use Intervention\Image\Facades\Image;
+use App\WikiSetting;
 
 /**
  * This can be run with for example:
@@ -37,31 +42,77 @@ class SetWikiLogo extends Job
 
         $wikis = Wiki::where($this->wikiKey, $this->wikiValue);
         if ($wikis->count() === 0) {
-            $this->fail(new \RuntimeException("Wiki not found for key={$this->wikiKey} and value={$this->wikiValue}"));
+            $this->fail(new \InvalidArgumentException("Wiki not found for key={$this->wikiKey} and value={$this->wikiValue}"));
             return;
         }
         elseif ($wikis->count() > 1) {
-            $this->fail(new \RuntimeException("Multiple Wikis matched for key={$this->wikiKey} and value={$this->wikiValue}"));
+            $this->fail(new \InvalidArgumentException("Multiple Wikis matched for key={$this->wikiKey} and value={$this->wikiValue}"));
             return;
         }
 
         $wiki = $wikis->first();
 
-        echo "Wiki ID:{$wiki->id}\n";
-        echo "Wiki sitename: {$wiki->sitename}\n";
-        echo "Wiki domain: {$wiki->domain}\n";
-        echo "logoPath: {$this->logoPath}\n";
+        // echo "Wiki ID:{$wiki->id}\n";
+        // echo "Wiki sitename: {$wiki->sitename}\n";
+        // echo "Wiki domain: {$wiki->domain}\n";
+        // echo "logoPath: {$this->logoPath}\n";
 
         // // Get the cloudy disk we use to store logos
-        // $storage = Storage::disk('gcs-public-static');
-        // if (! $storage instanceof Cloud) {
-        //     return response()->json('Invalid storage (not cloud)', 500);
-        // }
+        $storage = Storage::disk('gcs-public-static');
+        if (!$storage instanceof Cloud) {
+            # TODO: Use a more specific exception?
+            $this->fail(new \RuntimeException("Invalid storage (not cloud)"));
+            return;
+        }
 
-        // // Get a directory for storing all things relating to this site
-        // $logosDir = Wiki::getLogosDirectory($wiki->id);
-        // // Upload the local image to the cloud storage
-        // $storage::putFile($logosDir, new File($this->logoPath))
+        // Get a directory for storing all things relating to this site
+        $logosDir = Wiki::getLogosDirectory($wiki->id);
+        // Upload the local image to the cloud storage
+        $storage->putFileAs($logosDir, new File($this->logoPath), "raw.png");
+
+        // Store a conversion for the actual site logo
+        $reducedPath = $logosDir . '/135.png';
+        if ($storage->exists($reducedPath)) {
+            $storage->delete($reducedPath);
+        }
+        $storage->writeStream(
+            $reducedPath,
+            Image::make($storage->path($logosDir . "/raw.png"))->resize(135, 135)->stream()->detach()
+        );
+
+        // Store a favicon
+        $faviconPath = $logosDir . '/64.ico';
+        if ($storage->exists($faviconPath)) {
+            $storage->delete($faviconPath);
+        }
+        $storage->writeStream(
+            $faviconPath,
+            Image::make($storage->path($logosDir . "/raw.png"))->resize(64, 64)->stream()->detach()
+        );
+
+        // Get the urls
+        $logoUrl = $storage->url($reducedPath);
+        $faviconUrl = $storage->url($faviconPath);
+        // Append the time to the url so that client caches will be invalidated
+        $logoUrl .= '?u=' . time();
+        $faviconUrl .= '?u=' . time();
+
+        // Docs: https://www.mediawiki.org/wiki/Manual:$wgLogo
+        // WikiSetting::create(['wiki_id' => $wiki->id, 'name' => WikiSetting::wgLogo, 'value' => $logoUrl]);
+        WikiSetting::updateOrCreate(
+            ['wiki_id' => $wiki->id, 'name' => WikiSetting::wgLogo],
+            ['value' => $logoUrl]
+        );
+
+        // WikiSetting::updateOrCreate(
+        //     [
+        //         'wiki_id' => $wikiId,
+        //         'name' => 'wgLogo',
+        //     ],
+        //     [
+        //         'value' => $logoUrl,
+        //     ]
+        // );
 
         return; //safegaurd
     }
--- tests/Jobs/SetWikiLogoTest.php
+++ tests/Jobs/SetWikiLogoTest.php
@@ -9,19 +9,24 @@ use App\Jobs\DeleteQueryserviceNamespaceJob;
 use App\QueryserviceNamespace;
 use Illuminate\Support\Facades\DB;
 use App\WikiManager;
+use App\WikiSetting;
 use App\User;
 use App\Wiki;
 use Carbon\Carbon;
 use Illuminate\Contracts\Queue\Job;
 use App\Jobs\SetWikiLogo;
 use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Support\Facades\Storage;
+use Intervention\Image\Facades\Image;
+
 
 class SetWikiLogoTest extends TestCase
 {
     use DatabaseTransactions;
     use DispatchesJobs;
-    
-    public function assertJobFails(string $wikiKey, string $wikiValue, string $logoPath) {
+
+    public function assertJobFails(string $wikiKey, string $wikiValue, string $logoPath)
+    {
         $mockJob = $this->createMock(Job::class);
         $job = new SetWikiLogo($wikiKey, $wikiValue, $logoPath);
         $job->setJob($mockJob);
@@ -30,7 +35,8 @@ class SetWikiLogoTest extends TestCase
         $this->dispatchNow($job);
     }
 
-    public function assertJobSucceeds(string $wikiKey, string $wikiValue, string $logoPath) {
+    public function assertJobSucceeds(string $wikiKey, string $wikiValue, string $logoPath)
+    {
         $mockJob = $this->createMock(Job::class);
         $job = new SetWikiLogo($wikiKey, $wikiValue, $logoPath);
         $job->setJob($mockJob);
@@ -39,13 +45,55 @@ class SetWikiLogoTest extends TestCase
         $this->dispatchNow($job);
     }
 
-    public function testSetLogoInvalid()
+    public function testInvalidLogoPath()
     {
-        $this->assertJobFails( 'id', 1, "/path/to/logo" );
+        $this->assertJobFails('id', 1, "/path/to/logo");
     }
-    
-    public function testSetLogoValid()
+
+    public function testValidLogoPath()
     {
+        $wikiId = 1;
+        $storage = Storage::disk('gcs-public-static');
+        // delete the raw.png file form the cloud storage
+        $storage->delete('sites/bc7235a51e34c1d3ebfddeb538c20c71/logos/raw.png');
+        $this->assertJobSucceeds('id', $wikiId, __DIR__ . "/logo_200x200.png");
+        $this->assertTrue($storage->exists('sites/bc7235a51e34c1d3ebfddeb538c20c71/logos/raw.png'));
+    }
+
+    public function testLogoResize135()
+    {
+        $storage = Storage::disk('gcs-public-static');
+        // delete the 135.png file form the cloud storage
+        $storage->delete('sites/bc7235a51e34c1d3ebfddeb538c20c71/logos/135.png');
+        
         $this->assertJobSucceeds('id', 1, __DIR__ . "/logo_200x200.png");
+        $logo = Image::make($storage->path('sites/bc7235a51e34c1d3ebfddeb538c20c71/logos/135.png'));
+        $this->assertSame(135, $logo->height());
+        $this->assertSame(135, $logo->width());
+
+    }
+
+    public function testLogoResize64()
+    {
+        $storage = Storage::disk('gcs-public-static');
+        // delete the 64.ico file form the cloud storage
+        $storage->delete('sites/bc7235a51e34c1d3ebfddeb538c20c71/logos/64.ico');
+
+        $this->assertJobSucceeds('id', 1, __DIR__ . "/logo_200x200.png");
+        $logo = Image::make($storage->path('sites/bc7235a51e34c1d3ebfddeb538c20c71/logos/64.ico'));
+        $this->assertSame(64, $logo->height());
+        $this->assertSame(64, $logo->width());
+    }
+
+    public function testSettingsSet()
+    {
+        $storage = Storage::disk('gcs-public-static');
+        // delete the 135.png file form the cloud storage
+        $storage->delete('sites/bc7235a51e34c1d3ebfddeb538c20c71/logos/135.png');
+
+        $this->assertJobSucceeds('id', 1, __DIR__ . "/logo_200x200.png");
+        $wikiSetting = WikiSetting::firstWhere('id', 1);
+        var_dump($wikiSetting); die;
+        $this->assertSame($storage->path('sites/bc7235a51e34c1d3ebfddeb538c20c71/logos/135.png'), $wikiSetting->wgLogo);
     }
 }
