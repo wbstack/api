956815cca74914d8874047f6866a5ed8c8572696
Dat <dat.nguyen@wikimedia.de>
2022-07-29 18:12:01 +0200

    Add integration test for ForgotPassword and ResetPassword flow

---
+++ tests/Routes/User/ForgotAndResetPasswordIntegrationTest.php
@@ -0,0 +1,37 @@
+<?php
+
+
+namespace Tests\Routes\User;
+
+use App\Notifications\ResetPasswordNotification;
+use App\User;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Support\Facades\Notification;
+use Tests\TestCase;
+
+class ForgotAndResetPasswordIntegrationTest extends TestCase
+{
+    protected $forgot_route = 'user/forgotPassword';
+    protected $reset_route = 'user/resetPassword';
+
+    use DatabaseTransactions;
+
+    public function testForgotAndResetPassword()
+    {
+        Notification::fake();
+        $user = User::factory()->create(['email' => 'foo+bar@example.com']);
+        $this->json('POST', $this->forgot_route, ['email' => $user->email,]);
+        Notification::assertSentTo(
+            $user,
+            function (ResetPasswordNotification $notification) use ($user) {
+                $this->json('POST', $this->reset_route, [
+                    'email' => $user->email,
+                    'password' => 'AnyPassword122333',
+                    'password_confirmation' => 'AnyPassword122333',
+                    'token' => $notification->token
+                ])->assertStatus(200);
+                return true;
+            }
+        );
+    }
+}
