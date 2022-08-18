e8724946f3b74b45f0e7c92aee5f1aa99e28e947
toan <tobias.andersson@wikimedia.de>
2022-08-18 12:10:48 +0200

    Refactor /mine to include wikis, the count and the limit
    Bug: T315441

--- tests/Routes/Wiki/MineTest.php
+++ tests/Routes/Wiki/MineTest.php
@@ -4,10 +4,44 @@ namespace Tests\Routes\Wiki\Managers;
 
 use Tests\Routes\Traits\OptionsRequestAllowed;
 use Tests\TestCase;
+use App\User;
+use Illuminate\Support\Facades\Config;
+use App\Wiki;
+use App\WikiManager;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class MineTest extends TestCase
 {
     protected $route = 'wiki/mine';
 
     use OptionsRequestAllowed;
+    use DatabaseTransactions;
+
+    public function testMineDefault()
+    {
+        Config::set('wbstack.wiki_max_per_user', false);
+
+        $user = User::factory()->create(['verified' => true]);
+        $this->actingAs($user, 'api')
+            ->json('POST', $this->route, [])
+            ->assertStatus(200)
+            ->assertJsonFragment([ "wikis" => [], "count" => 0, "limit" =>  false]);
+    }
+
+    public function testMineWithWikis()
+    {
+        Config::set('wbstack.wiki_max_per_user', 1);
+
+        $user = User::factory()->create(['verified' => true]);
+        $wiki = Wiki::factory()->create();
+        WikiManager::factory()->create(['wiki_id' => $wiki->id, 'user_id' => $user->id]);
+
+        $content = $this->actingAs($user, 'api')
+            ->json('POST', $this->route, [])
+            ->assertStatus(200)
+            ->assertJson([ "wikis" => [], "count" => 1, "limit" =>  1])->getContent();
+
+
+        $this->assertEquals( $wiki->id, json_decode($content)->wikis[0]->id );
+    }
 }
