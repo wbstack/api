5391588695341a6784c6917714284bb480158106
Tobias <tobias.andersson@wikimedia.de>
2022-09-01 09:23:32 +0200

    Add bulk create invite command (#516)
    * Add bulk create invite command
    
    Co-authored-by: Deniz Erdogan <deniz.erdogan@wikimedia.de>

---
+++ app/Helper/InviteHelper.php
@@ -0,0 +1,40 @@
+<?php
+namespace App\Helper;
+
+class InviteHelper
+{
+
+    private $segments;
+    private $segmentLength;
+    private $prefix;
+
+    public function __construct( int $numSegments = 2, int $segmentLength = 4)
+    {
+        $this->prefix = "wbcloud-";
+        $this->segments = $numSegments;
+        $this->segmentLength = $segmentLength;
+    }
+
+    private function generateSegment( int &$counter ) {
+        $segment = "";
+
+        for($i = 0; $i < $this->segmentLength; $i++) {
+            $segment .= rand(0, 9); 
+        }
+
+        $counter++;
+        
+        if($counter < $this->segments) {
+            return $segment . '-' . $this->generateSegment( $counter );
+        }
+
+        return $segment;
+    }
+
+    public function generate(): string {
+        $counter = 0;
+        return $this->prefix . $this->generateSegment( $counter );
+    }
+
+    
+}
