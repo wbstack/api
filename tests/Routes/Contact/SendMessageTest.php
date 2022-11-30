bf76c89743d880e40a66ab0e778c44d632aa1eab
Deniz Erdogan <deniz.erdogan@wikimedia.de>
2022-11-30 21:07:21 +0100

    add WIP failing test case

--- tests/Routes/Contact/SendMessageTest.php
+++ tests/Routes/Contact/SendMessageTest.php
@@ -8,8 +8,43 @@ class SendMessageTest extends TestCase
 {
     protected $route = 'contact/sendMessage';
 
+    protected $postDataTemplate = [
+        'name'           => '',
+        'contactDetails' => '',
+        'subject'        => '',
+        'message'        => '',
+        'recaptcha'      => '',
+    ];
+
+    protected $validSubjects = [
+        'general-question',
+        'feature-request',
+        'report-a-problem',
+        'give-feedback',
+        'other',
+    ];
+
+    public function testSendMessage_NoData()
+    {
+        $data = $this->postDataTemplate;
+
+        $response = $this->json('POST', $this->route, $data);
+        $response->assertStatus(400);
+    }
+
+    public function testSendMessage_InvalidDataSubject()
+    {
+        $data = $this->postDataTemplate;
+
+        $response = $this->json('POST', $this->route, $data);
+        $response->assertStatus(400);
+    }
+
     public function testSendMessage_Success()
     {
+        $data = $this->postDataTemplate;
 
+        $response = $this->json('POST', $this->route, $data);
+        $response->assertStatus(200);
     }
 }
