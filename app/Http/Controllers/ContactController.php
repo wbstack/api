cac0d945199105534e8ce36ff6c6f8bcba24813e
Deniz Erdogan <deniz.erdogan@wikimedia.de>
2022-12-02 17:17:31 +0100

    Clean up ContactController.php

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -83,26 +83,4 @@ class ContactController extends Controller
 
         return Validator::make($data, $validation);
     }
-
-    /**
-     * Send a reset link to the given user.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Http\JsonResponse
-     */
-    public function sendResetLinkEmail(Request $request)
-    {
-        $this->validateEmail($request);
-
-        // We will send the password reset link to this user. Once we have attempted
-        // to send the link, we will examine the response then see the message we
-        // need to show to the user. Finally, we'll send out a proper response.
-        $response = $this->broker()->sendResetLink(
-            $this->credentials($request)
-        );
-
-        return $response == Password::RESET_LINK_SENT
-            ? $this->sendResetLinkResponse($request, $response)
-            : $this->sendResetLinkFailedResponse($request, $response);
-    }
 }
