155fa36d09780b314e8ee7315ccf93b8f4ac4275
Thomas Arrow <thomas.arrow_ext@wikimedia.de>
2022-12-13 13:51:18 +0000

    Add additional tests and fix existing

--- tests/Routes/Contact/SendMessageTest.php
+++ tests/Routes/Contact/SendMessageTest.php
@@ -2,13 +2,20 @@
 
 namespace Tests\Routes\Contact;
 
+use App\Notifications\ContactNotification;
+use App\User;
+use Illuminate\Contracts\Mail\Mailable;
+use Illuminate\Notifications\AnonymousNotifiable;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Notification;
 use Tests\TestCase;
 
 class SendMessageTest extends TestCase
 {
     protected $route = 'contact/sendMessage';
 
-    protected $postDataTemplate = [
+    protected $postDataTemplateEmpty = [
         'name'           => '',
         'contactDetails' => '',
         'subject'        => '',
@@ -26,7 +33,7 @@ class SendMessageTest extends TestCase
 
     public function testSendMessage_NoData()
     {
-        $data = $this->postDataTemplate;
+        $data = $this->postDataTemplateEmpty;
 
         $response = $this->json('POST', $this->route, $data);
         $response->assertStatus(400);
@@ -34,17 +41,48 @@ class SendMessageTest extends TestCase
 
     public function testSendMessage_InvalidDataSubject()
     {
-        $data = $this->postDataTemplate;
-
+        $data = $this->postDataTemplateEmpty;
+        $data['message'] = "Hi!";
+        $data['subject'] = "not-valid";
         $response = $this->json('POST', $this->route, $data);
         $response->assertStatus(400);
     }
 
     public function testSendMessage_Success()
     {
-        $data = $this->postDataTemplate;
+        Notification::fake();
+        $data = [
+            'name'           => 'foo',
+            'contactDetails' => 'bar',
+            'subject'        => 'general-question',
+            'message'        => 'baz',
+            'recaptcha'      => 'fake-token',
+        ];
 
         $response = $this->json('POST', $this->route, $data);
         $response->assertStatus(200);
+        $recipient = config('app.contact-mail-recipient');
+        Notification::assertSentTo(new AnonymousNotifiable(), ContactNotification::class, function ($notification) {
+            $this->assertSame(
+                "contact-general-question@wikibase.cloud",
+                $notification->toMail(new AnonymousNotifiable())->from[0]
+            );
+            return true;
+        });
+    }
+
+    public function testSendMessage_RecaptchaFailure()
+    {
+        $data = [
+            'name'           => 'foo',
+            'contactDetails' => 'bar',
+            'subject'        => 'general-question',
+            'message'        => 'baz',
+            'recaptcha'      => 'fake-token',
+        ];
+
+        putenv('PHPUNIT_RECAPTCHA_CHECK=1');
+        $response = $this->json('POST', $this->route, $data);
+        $response->assertStatus(401);
     }
 }
