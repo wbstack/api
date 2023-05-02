2fc3de096466f9894f5d4c78b2fe19668b802918
Frederik Ring <frederik.ring@wikimedia.de>
2023-05-02 11:41:14 +0200

    feat: query for running mediawiki job

--- app/Jobs/ProcessMediaWikiJobsJob.php
+++ app/Jobs/ProcessMediaWikiJobsJob.php
@@ -6,6 +6,8 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Contracts\Queue\ShouldBeUnique;
 use Illuminate\Queue\InteractsWithQueue;
+use Maclof\Kubernetes\Client;
+use Maclof\Kubernetes\Models\Job as KubernetesJob;
 
 class ProcessMediaWikiJobsJob implements ShouldQueue, ShouldBeUnique
 {
@@ -23,8 +25,25 @@ class ProcessMediaWikiJobsJob implements ShouldQueue, ShouldBeUnique
         return $this->wikiDomain;
     }
 
-    public function handle (): void
+    public function handle (Client $kubernetesClient): void
     {
+        $mwPod = $kubernetesClient->pods()->setFieldSelector([
+            'status.phase' => 'Running'
+        ])->setLabelSelector([
+            'app.kubernetes.io/name' => 'mediawiki',
+            'app.kubernetes.io/component' => 'app-backend'
+        ])->first();
+
+        if (!$mwPod) {
+            throw new Exception(
+                'Unable to find a running MediaWiki pod in the cluster, '.
+                'cannot continue.'
+            );
+        }
+
+        $job = new KubernetesJob([]);
+        $kubernetesClient->jobs->create($job);
         return;
     }
+
 }
