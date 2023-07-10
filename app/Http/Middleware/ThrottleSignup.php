09e464aa259479df10c92e0ec5ac2f0a3f712159
Frederik Ring <frederik.ring@wikimedia.de>
2023-07-10 15:36:14 +0200

    fix: use correct log level semantics

--- app/Http/Middleware/ThrottleSignup.php
+++ app/Http/Middleware/ThrottleSignup.php
@@ -19,7 +19,7 @@ class ThrottleSignup
         $lookback = Carbon::now()->sub(new \DateInterval($range));
         $recentSignups = User::where('created_at', '>=', $lookback)->count();
         if ($recentSignups >= intval($limit)) {
-            Log::error("WARN_SIGNUP_THROTTLED: Given limit of '$limit' in range '$range' was exceeded, attempted account creation was blocked.");
+            Log::warning("WARN_SIGNUP_THROTTLED: Given limit of '$limit' in range '$range' was exceeded, attempted account creation was blocked.");
             return response()->json(
                 ["error" => "Due to high load, we're currently not able to create an account for you. Please try again tomorrow or reach out through our contact page."],
                 429
