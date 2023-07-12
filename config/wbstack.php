13142013f85e36bb2b0e9639740942b3546ba296
Frederik Ring <frederik.ring@wikimedia.de>
2023-07-12 12:32:20 +0200

    fix: add missing prefix for env var keys

--- config/wbstack.php
+++ config/wbstack.php
@@ -19,7 +19,7 @@ return [
     'elasticsearch_host' => env('ELASTICSEARCH_HOST', false),
     'elasticsearch_enabled_by_default' => env('WBSTACK_ELASTICSEARCH_ENABLED_BY_DEFAULT', false),
 
-    'signup_throttling_limit' => env('SIGNUP_THROTTLING_LIMIT', ''),
-    'signup_throttling_range' => env('SIGNUP_THROTTLING_RANGE', ''),
+    'signup_throttling_limit' => env('WBSTACK_SIGNUP_THROTTLING_LIMIT', ''),
+    'signup_throttling_range' => env('WBSTACK_SIGNUP_THROTTLING_RANGE', ''),
 
 ];
