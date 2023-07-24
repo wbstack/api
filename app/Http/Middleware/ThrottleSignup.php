ec00b6deb8c7737d917cd767976a98c12adbb91d
Frederik Ring <frederik.ring@gmail.com>
2023-07-24 16:02:13 +0200

    feat: report exception instead of logging when throttling account creation (#620)
    * feat: report exception instead of logging when throttling account creation
    
    * docs: update changelog

--- app/Http/Middleware/ThrottleSignup.php
+++ app/Http/Middleware/ThrottleSignup.php
@@ -6,7 +6,6 @@ use Closure;
 use Carbon\Carbon;
 use App\User;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
 
 class ThrottleSignup
 {
@@ -19,7 +18,7 @@ class ThrottleSignup
         $lookback = Carbon::now()->sub(new \DateInterval($range));
         $recentSignups = User::where('created_at', '>=', $lookback)->count();
         if ($recentSignups >= intval($limit)) {
-            Log::warning("WARN_SIGNUP_THROTTLED: Given limit of '$limit' in range '$range' was exceeded, attempted account creation was blocked.");
+            report("Attempted account creation was blocked because given limits were exceeded.");
             return response()->json(
                 ["errors" => "Due to high load, we're currently not able to create an account for you. Please try again tomorrow or reach out through our contact page."],
                 503
