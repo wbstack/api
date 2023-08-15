c48fd2e4803db7c047672532a02cb2b7bdd6b699
Deniz Erdogan <deniz.erdogan@wikimedia.de>
2023-08-15 20:38:53 +0200

    feat(console): add Command for changing user mail

---
+++ app/Console/Commands/User/ChangeEmail.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Console\Commands\User;
+
+use App\User;
+use Illuminate\Console\Command;
+use App\Jobs\UserVerificationCreateTokenAndSendJob;
+
+class ChangeEmail extends Command
+{
+    protected $signature = 'wbs-user:change-email';
+
+    protected $description = 'Change e-mail address for user';
+
+    public function handle(): int
+    {
+        do {
+            $emailOld = $this->ask('Current user address');
+            $user = User::whereEmail($emailOld)->first();
+
+            if ($user) {
+                $this->line("Found a user for '$emailOld'");
+            } else {
+                $this->warn("Did not find a user for '$emailOld' Please try again.");
+            }
+        } while(!$user);
+
+        do {
+            $emailNew = $this->ask('New user address');
+            
+            if ($emailNew == $emailOld) {
+                $this->warn("New email matches current email. Please provide a different address.");
+            }
+        } while ($emailNew == $emailOld);
+
+        if (! $this->confirm("Confirm: changing user mail address '$emailOld' to '$emailNew'")) {
+            $this->warn("Aborted changing user email address");
+            return 1;
+        }
+
+        $user->email = $emailNew;
+        $user->verified = false;
+
+        if ($user->save()) {
+            UserVerificationCreateTokenAndSendJob::newForReverification($user)->handle();
+
+            $this->info("Successfully changed user email '$emailOld' to '$emailNew'");
+            $this->line("Note: a verification mail was sent to the new address ('$emailNew').");
+
+            return 0;
+        } else {
+            $this->error("Failed to change user email '$emailOld' to '$emailNew'");
+            return 2;
+        }
+    }
+}
---
+++ tests/Commands/User/ChangeEmailTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Tests\Commands;
+
+use App\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+use App\Notifications\EmailReverificationNotification;
+use Illuminate\Support\Facades\Notification;
+
+class ChangeEmailTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test()
+    {
+        Notification::fake();
+
+        $emailOld = 'old@example.com';
+        $emailNew = 'new@example.com';
+
+        $oldUser = new User(['email' => $emailOld, 'password' => 'worldsstrongestpassword']);
+        $oldUser->save();
+
+        $this->artisan('wbs-user:change-email')
+            ->expectsQuestion('Current user address', $emailNew)
+            ->expectsOutput("Did not find a user for '$emailNew' Please try again.")
+            ->expectsQuestion('Current user address', $emailOld)
+            ->expectsOutput("Found a user for '$emailOld'")
+            ->expectsQuestion('New user address', $emailOld)
+            ->expectsOutput("New email matches current email. Please provide a different address.")
+            ->expectsQuestion('New user address', $emailNew)
+            ->expectsConfirmation("Confirm: changing user mail address '$emailOld' to '$emailNew'", "yes")
+            ->expectsOutput("Successfully changed user email '$emailOld' to '$emailNew'")
+            ->expectsOutput("Note: a verification mail was sent to the new address ('$emailNew').")
+            ->assertExitCode(0);
+
+        $newUser = User::firstWhere('email', $emailNew);
+
+        $this->assertSame($oldUser->id, $newUser->id);
+        $this->assertSame($newUser->email, $emailNew);
+        $this->assertSame($newUser->verified, 0);
+
+        Notification::assertSentTo([$newUser], EmailReverificationNotification::class);
+    }
+}
