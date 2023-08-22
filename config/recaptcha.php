035fb1a83844cb71e8199a1aae7edd0f2f034287
Deniz Erdogan <deniz.erdogan@wikimedia.de>
2023-08-22 20:42:03 +0200

    remoce unneeded site key config value

--- config/recaptcha.php
+++ config/recaptcha.php
@@ -2,6 +2,5 @@
 
 return [
     'secret_key' => env('RECAPTCHA_V3_SECRET_KEY', 'config/recaptcha.php: no-secret-key-set!'),
-    'site_key'   => env('RECAPTCHA_V3_SITE_KEY', 'config/recaptcha.php: no-site-key-set!'),
     'min_score'  => env('RECAPTCHA_V3_MIN_SCORE', 0.5),
 ];
