f9356fa0d961ae02e6b76cd0ee03e0bb39af5530
Deniz Erdogan <deniz.erdogan@wikimedia.de>
2023-08-22 20:46:02 +0200

    Add ReCaptchaServiceProvider

---
+++ app/Providers/ReCaptchaServiceProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use App\Rules\ReCaptchaValidation;
+
+class ReCaptchaServiceProvider extends ServiceProvider
+{
+   /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->bind(ReCaptchaValidation::class, function($app) {
+            return new ReCaptchaValidation(
+                config('recaptcha.secret'),
+                config('recaptcha.min_score'),
+                config('app.url')
+            );
+        });
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return [ReCaptchaValidation::class];
+    }
+}
--- config/app.php
+++ config/app.php
@@ -180,6 +180,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\KubernetesClientServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        App\Providers\ReCaptchaServiceProvider::class,
 
         /*
          * Imported from LUMEN
