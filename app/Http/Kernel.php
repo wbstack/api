9df107885ca176d2e29d2455fc99c1c1137bc909
Frederik Ring <frederik.ring@wikimedia.de>
2023-08-23 17:26:34 +0200

    feat(http): allow trusted proxies to be defined

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -15,6 +15,7 @@ class Kernel extends HttpKernel
      */
     protected $middleware = [
         \Fruitcake\Cors\HandleCors::class,
+        \Illuminate\Http\Middleware\TrustProxies::class,
     ];
 
     /**
--- config/app.php
+++ config/app.php
@@ -252,5 +252,4 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
 
     ],
-
 ];
