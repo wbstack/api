5ab6986a3c7cbd78a51ac3c19bc2bd4844044da4
Deniz Erdogan <deniz.erdogan@wikimedia.de>
2023-10-12 17:59:38 +0200

    formatting

--- tests/Helper/DomainHelperTest.php
+++ tests/Helper/DomainHelperTest.php
@@ -11,27 +11,27 @@ class DomainHelperTest extends TestCase {
      */
     public function provideUnicodeDomains(): array {
         return [
-            'example IDNA encoding' => [
+            'Example IDNA encoding' => [
                 'bücher.example',
                 'xn--bcher-kva.example',
             ],
-            'example IDNA encoding #2 - Latin-1' => [
+            'Example IDNA encoding #2 - Latin-1' => [
                 'então.carolinadoran.com',
                 'xn--ento-ioa.carolinadoran.com'
             ],
-            'example IDNA encoding #3 - Greek' => [
+            'Example IDNA encoding #3 - Greek' => [
                 'άλφα.wikibase.cloud',
                 'xn--hxak3a7b.wikibase.cloud'
             ],
-            'example IDNA encoding #4 - Japanese' => [
+            'Example IDNA encoding #4 - Japanese' => [
                 'ドメイン名例.wikibase.cloud',
                 'xn--eckwd4c7cu47r2wf.wikibase.cloud'
             ],
-            'Prevent double-encoding of "münchen.wikibase.cloud"' => [
+            'No double-encoding of "münchen.wikibase.cloud"' => [
                 'xn--mnchen-3ya.wikibase.cloud',
                 'xn--mnchen-3ya.wikibase.cloud',
             ],
-            'Prevent double-encoding of "então.carolinadoran.com"' => [
+            'No double-encoding of "então.carolinadoran.com"' => [
                 'xn--ento-ioa.carolinadoran.com',
                 'xn--ento-ioa.carolinadoran.com',
             ],
@@ -47,27 +47,27 @@ class DomainHelperTest extends TestCase {
      */
     public function provideAsciiDomains(): array {
         return [
-            'example IDNA decoding' => [
+            'Example IDNA decoding' => [
                 'xn--bcher-kva.example',
                 'bücher.example',
             ],
-            'example IDNA decoding #2 - Latin-1' => [
+            'Example IDNA decoding #2 - Latin-1' => [
                 'xn--ento-ioa.carolinadoran.com',
                 'então.carolinadoran.com',
             ],
-            'example IDNA decoding #3 - Greek' => [
+            'Example IDNA decoding #3 - Greek' => [
                 'xn--hxak3a7b.wikibase.cloud',
                 'άλφα.wikibase.cloud',
             ],
-            'example IDNA decoding #4 - Japanese' => [
+            'Example IDNA decoding #4 - Japanese' => [
                 'xn--eckwd4c7cu47r2wf.wikibase.cloud',
                 'ドメイン名例.wikibase.cloud',
             ],
-            'Domain in unicode is stays the same' => [
+            'Domain in unicode stays the same' => [
                 'münchen.wikibase.cloud',
                 'münchen.wikibase.cloud',
             ],
-            'Domain in unicode is stays the same #2' => [
+            'Domain in unicode stays the same #2' => [
                 'então.carolinadoran.com',
                 'então.carolinadoran.com',
             ],
