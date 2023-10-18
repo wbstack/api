4d50ac9fe120245f79f303bdfab388fd6bbaefbf
Frederik Ring <frederik.ring@wikimedia.de>
2023-10-18 13:12:23 +0200

    refactor(qs): refactor qs controller for readability

--- app/Http/Controllers/Backend/QsController.php
+++ app/Http/Controllers/Backend/QsController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
+use App\QsBatch;
+use App\EventPageUpdate;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -11,15 +13,9 @@ class QsController extends Controller
 {
     public function getBatches(Request $request): \Illuminate\Http\Response
     {
-        $notDoneBatches = null;
-        $batches = [];
-        $returnCollection = [];
-
-        DB::transaction(function () use (&$notDoneBatches, &$batches, &$returnCollection) {
-            $notDoneBatches = \App\QsBatch::where('done', 0)->with(['wiki', 'wiki.wikiQueryserviceNamespace'])->get();
-
+        return DB::transaction(function () {
             // Get ID of the latest batch that we have created (or 0)
-            $maxIdBatch = \App\QsBatch::orderBy('id', 'desc')->first();
+            $maxIdBatch = QsBatch::orderBy('id', 'desc')->first();
             if ($maxIdBatch) {
                 $batchesUpToEventId = $maxIdBatch->eventTo;
             } else {
@@ -29,7 +25,7 @@ class QsController extends Controller
 
             // Get events after the point that batch was created
             // TODO maybe filter by NS here?
-            $events = \App\EventPageUpdate::where('id', '>', $batchesUpToEventId)->get();
+            $events = EventPageUpdate::where('id', '>', $batchesUpToEventId)->get();
             $wikiBatchesEntities = [];
             $lastEventId = 0;
             foreach ($events as $event) {
@@ -45,6 +41,9 @@ class QsController extends Controller
                 }
             }
 
+            /** @var Collection $batches */
+            $newBatches = [];
+            $notDoneBatches = QsBatch::where('done', 0)->with(['wiki', 'wiki.wikiQueryserviceNamespace'])->get();
             // Inset the newly created batches into the table...
             foreach ($wikiBatchesEntities as $wikiId => $entityBatch) {
 
@@ -58,7 +57,7 @@ class QsController extends Controller
                 }
 
                 // Insert the new batch
-                $batch = \App\QsBatch::create([
+                $batch = QsBatch::create([
                     'done' => 0,
                     'eventFrom' => $batchesUpToEventId,
                     'eventTo'=> $lastEventId,
@@ -67,13 +66,12 @@ class QsController extends Controller
                 ]);
                 // TODO to the loading on all batches at once? :D
                 $batch->load(['wiki', 'wiki.wikiQueryserviceNamespace']);
-                $batches[] = $batch;
+                $newBatches[] = $batch;
             }
 
             // $batches is all the batches, but lets just return one
             // and for now mark it as done
-            /** @var Collection $batches */
-            $sorted = collect($batches)->sortBy('id');
+            $sorted = collect($newBatches)->sortBy('id');
 
             if ($sorted->isEmpty()) {
                 /**
@@ -85,27 +83,21 @@ class QsController extends Controller
             }
 
             $first = $sorted->first();
-
             if ($first === null) {
-                $returnCollection = [];
-            } else {
-                $first->update(['done' => 1]);
-                $returnCollection = [$first];
+                return response([]);
             }
+
+            $first->update(['done' => 1]);
+            return response([$first]);
         });
 
-        return response(collect($returnCollection));
     }
 
     public function markBatchesDone(Request $request): \Illuminate\Http\Response
     {
         $rawBatches = $request->input('batches');
         $batches = explode(',', $rawBatches);
-        foreach ($batches as $batch) {
-            \App\QsBatch::where('id', $batch)
-                ->update(['done' => 1]);
-        }
-
+        QsBatch::whereIn('id', $batches)->update('done', 1);
         return response(1);
     }
 
@@ -113,11 +105,7 @@ class QsController extends Controller
     {
         $rawBatches = $request->input('batches');
         $batches = explode(',', $rawBatches);
-        foreach ($batches as $batch) {
-            \App\QsBatch::where('id', $batch)
-                ->update(['done' => 0]);
-        }
-
+        QsBatch::whereIn('id', $batches)->update('done', 0);
         return response(1);
     }
 }
