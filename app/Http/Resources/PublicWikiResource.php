b662d334978ee7664679245c178c04aafe202347
Deniz Erdogan <91744937+deer-wmde@users.noreply.github.com>
2023-10-25 10:25:17 +0200

    fix(domains): store domains in IDN format (#652)
    * add fomvasss/laravel-punycode
    
    * Add domain punycode transcoding
    
    * add static punycode methods to WikiDomain
    
    * use WikiDomain transcoding methods
    
    * add punycode decoding to PublicWikiResource
    
    * remove punycode decoding as per ADR
    
    * Remove punycode package
    
    * Use native php IDN functions
    
    * simplify DomainHelper as we are not using the info right now
    
    * formatting
    
    * add emoji test
    
    * add Wiki model accessor for decoded domain
    
    * add domain_decoded field to PublicWikiResource

--- app/Http/Resources/PublicWikiResource.php
+++ app/Http/Resources/PublicWikiResource.php
@@ -3,6 +3,7 @@
 namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
+use App\WikiDomain;
 
 class PublicWikiResource extends JsonResource
 {
@@ -13,6 +14,7 @@ class PublicWikiResource extends JsonResource
             'id' => $this->id,
             'description' => $this->description,
             'domain' => $this->domain,
+            'domain_decoded' => $this->domain_decoded,
             'sitename' => $this->sitename,
             'wiki_site_stats' => $this->wikiSiteStats,
             'logo_url' => $logoSetting ? $logoSetting->value : null
