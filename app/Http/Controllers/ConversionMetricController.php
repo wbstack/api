5e10987eb6b57fa312c3edcabd6a759cf5145b21
Perside Rosalie <perside.rosalie@wikimedia.de>
2023-10-25 16:52:46 +0200

    Create Conversion metrics and add an endpoint (#655)
    * Create Conversion metrics and add an endpoint
    
    * Add comments to the controller methods
    
    * Add ConversionMetric Tests
    
    * Add null to metric for rows with no data
    
    * Remove unused methods from the controller
    
    * added more changes
    
    * Adjustments to tests and functionality
    
    The commit removes the usage of the download function
    in place of an normal response call. So far we didn't
    temoporarily save the csv to disk which is necessary for
    using this method[1]. Instead this builds the csv in the
    output buffer[2] and then sends the contents of that.
    
    This therefore also adjusts the tests to remove the
    dependency on the mocked download method and instead
    relies on a single high-level test that not only checks
    the status and the error but also loosely checks that some
    of the expected content is there.
    
    We also make some adjustments to parsing the data from
    models which aren't guaranteed to be populated for all
    wikis since the stats collection for them may not have run
    
    Finally this commit slightly renames some of the headers
    of the columns for clarity.
    
    [1] https://laravel.com/docs/8.x/responses#file-downloads
    [2] https://www.php.net/manual/en/book.outcontrol.php
    
    * Add JSON output and more test coverage using this
    
    * Additional Test coverage of types of wiki
    
    * Add tests covering active use count
    
    * Update app/Http/Controllers/ConversionMetricController.php
    
    Co-authored-by: Deniz Erdogan <91744937+deer-wmde@users.noreply.github.com>
    
    * Update app/Http/Controllers/ConversionMetricController.php
    
    Co-authored-by: Deniz Erdogan <91744937+deer-wmde@users.noreply.github.com>
    
    * Add test and fix handling of empty lifecycleEvents
    
    * Fix echo and add datetime to csv filename
    
    ---------
    
    Co-authored-by: Thomas Arrow <thomas.arrow@wikimedia.de>
    Co-authored-by: Thomas Arrow <tarrow@users.noreply.github.com>
    Co-authored-by: Deniz Erdogan <91744937+deer-wmde@users.noreply.github.com>

---
+++ app/Http/Controllers/ConversionMetricController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Wiki;
+use Carbon\CarbonImmutable;
+use Illuminate\Http\Response;
+use Illuminate\Http\Request;
+
+class ConversionMetricController extends Controller
+{
+
+    private string $fileName="conversion_metric_for_all_wikis.csv";
+
+    /**
+     * Produce a downloadable csv file with conversion metrics for all wikis.
+     */
+    public function index(Request $request)
+    {
+        $allWikis = Wiki::all();
+        $current_date = CarbonImmutable::now();
+        $output = [];
+        
+
+        foreach ($allWikis as $wiki) {
+            $lifecycleEvents = $wiki->wikiLifecycleEvents()->first();
+            $wikiLastEditedTime = null;
+            $wikiFirstEditedTime = null;
+            if (!empty($lifecycleEvents)  ) {
+                if ($lifecycleEvents['last_edited']) {
+                    $wikiLastEditedTime = CarbonImmutable::parse($lifecycleEvents['last_edited']);    
+                }
+                if ($lifecycleEvents['first_edited']) {
+                    $wikiFirstEditedTime = CarbonImmutable::parse($lifecycleEvents['first_edited']);
+                }
+            }
+            $time_before_wiki_abandoned_days = null;
+            $time_to_engage_days = null;
+
+            if (!is_null($wikiLastEditedTime) && ($current_date->diffInDays($wikiLastEditedTime) >= 90)) {
+                $time_before_wiki_abandoned_days = $wikiLastEditedTime->diffInDays($wiki->created_at);
+            }
+            if ($wikiFirstEditedTime !== null) {
+                $time_to_engage_days = $wikiFirstEditedTime->diffInDays($wiki->created_at);
+            }
+            $wiki_number_of_editors = $wiki->wikiSiteStats()->first()['activeusers'] ?? null;
+
+            $output[] = [
+                'domain' => $wiki->domain,
+                'time_to_engage_days' => $time_to_engage_days,
+                'time_before_wiki_abandoned_days' => $time_before_wiki_abandoned_days,
+                'number_of_active_editors' => $wiki_number_of_editors
+            ];
+
+        }
+
+        if ( $request->wantsJson() ) {
+            return response()->json( $output );
+        }
+
+        return $this->returnCsv($output);
+
+    }
+
+    private function returnCsv( $output ) {
+        ob_start();
+		$handle = fopen('php://output', 'r+');
+        fputcsv($handle, ['domain_name', 'time_to_engage_days', 'time_before_wiki_abandoned_days', 'number_of_active_editors']);
+        foreach ($output as $wikiMetrics) {
+            fputcsv($handle, array_values($wikiMetrics));
+        }
+        $csv = ob_get_clean();
+        return response($csv, 200, [
+			'Content-Type' => 'text/csv',
+			'Content-Disposition' => 'attachment;filename='.CarbonImmutable::now()->toIso8601String().'-'.$this->fileName
+		]);;
+    }
+}
