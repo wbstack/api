0b3a876af939052c89d1bffdfc511ecc68a79f98
Deniz Erdogan <deniz.erdogan@wikimedia.de>
2023-11-21 11:37:20 +0100

    Revert "change stack logging driver"
    This reverts commit 137f08d3eb38401490ee13d5284719e9de889eee.

--- config/logging.php
+++ config/logging.php
@@ -36,7 +36,7 @@ return [
     'channels' => [
         'stack' => [
             'driver' => 'stack',
-            'channels' => ['daily', 'stderr'],
+            'channels' => ['daily'],
             'ignore_exceptions' => false,
         ],
 
