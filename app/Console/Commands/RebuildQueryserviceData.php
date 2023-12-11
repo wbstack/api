612cffb06a7d789ddaa276d4947fbeda25ac8936
Frederik Ring <frederik.ring@wikimedia.de>
2023-12-11 11:06:46 +0100

    feat: fetch entity ids from mediawiki api

--- app/Console/Commands/RebuildQueryserviceData.php
+++ app/Console/Commands/RebuildQueryserviceData.php
@@ -4,6 +4,8 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
 use App\Wiki;
 use App\WikiSetting;
 use App\Jobs\TemporaryDummyJob;
@@ -33,12 +35,16 @@ class RebuildQueryserviceDataCommand extends Command
      * @return void
      */
 
-     private $chunkSize;
+    protected int $chunkSize;
+    protected int $apiPageSize;
+    protected string $apiUrl;
 
     public function __construct()
     {
         parent::__construct();
         $this->chunkSize = Config::get('wbstack.qs_rebuild_chunk_size');
+        $this->apiUrl = getenv('PLATFORM_MW_BACKEND_HOST').'/w/api.php';
+        $this->apiPageSize = 500;
     }
 
     /**
@@ -49,14 +55,23 @@ class RebuildQueryserviceDataCommand extends Command
     public function handle()
     {
         $wikiDomain = $this->option('domain');
+        $exitCode = 0;
 
         $wikis = $wikiDomain
             ? Wiki::where(['domain' => $wikiDomain])->get()
             : Wiki::query()->get();
 
         foreach ($wikis as $wiki) {
-            $entities = $this->getEntitiesForWiki($wiki->domain);
-            $sparqlUrl = $this->getSparqlUrl($wiki->wikiQueryserviceNamespace()->namespace);
+            try {
+                $entities = $this->getEntitiesForWiki($wiki);
+                $sparqlUrl = $this->getSparqlUrl($wiki);
+            } catch (\Exception $ex) {
+                Log::error(
+                    'Failed to get prerequisites for enqueuing wiki '.$wiki->domain.', will not dispatch jobs.'
+                );
+                $exitCode = 1;
+                break;
+            }
 
             $entityChunks = array_chunk($entities, $this->chunkSize);
             foreach ($entityChunks as $entityChunk) {
@@ -70,7 +85,7 @@ class RebuildQueryserviceDataCommand extends Command
             }
         }
 
-        return 0;
+        return $exitCode;
     }
 
     private function getEntitiesForWiki (Wiki $wiki): array
@@ -83,20 +98,61 @@ class RebuildQueryserviceDataCommand extends Command
             ? $this->fetchPagesInNamespace($wiki->domain, self::NAMESPACE_LEXEME)
             : [];
 
-        return array_merge($items, $properties, $lexemes);
+        $merged = array_merge($items, $properties, $lexemes);
+        return $this->stripPrefixes($merged);
     }
 
-    private function getSparqlUrl (string $queryserviceNamespace): string
+    private static function getSparqlUrl (Wiki $wiki): string
     {
-        return 'http://queryservice.default.svc.cluster.local:9999/bigdata/namespace/'.$queryserviceNamespace.'/sparql';
+        $ns = $wiki->wikiQueryserviceNamespace()->namespace;
+        return 'http://queryservice.default.svc.cluster.local:9999/bigdata/namespace/'.$ns.'/sparql';
     }
 
     private function fetchPagesInNamespace(string $wikiDomain, int $namespace): array
     {
-        return [];
+        $titles = [];
+        $cursor = '';
+        while (true)
+        {
+            $response = Http::withHeaders([
+                'host' => $wikiDomain
+            ])->get(
+                $this->apiUrl,
+                [
+                    'action' => 'query',
+                    'list' => 'allpages',
+                    'apnamespace' => $namespace,
+                    'apcontinue' => $cursor,
+                    'aplimit' => $this->apiPageSize,
+                ],
+            );
+
+            if ($response->failed()) {
+                throw new \Exception('Failed to fetch allpages for wiki '.$wikiDomain);
+            }
+
+            $jsonResponse = $response->json();
+            $error = data_get($jsonResponse, 'error');
+            if ($error !== null) {
+                throw new \Exception('Error fetching allpages for wiki '.$wikiDomain.': '.$error);
+            }
+
+            $pages = data_get($jsonResponse, 'query.allpages', []);
+            $titles = array_merge($titles, array_map(function (array $page) {
+                return $page['title'];
+            }, $pages));
+
+            $nextCursor = data_get($jsonResponse, 'continue.apcontinue');
+            if ($nextCursor === null) {
+                break;
+            }
+            $cursor = $nextCursor;
+        }
+
+        return $titles;
     }
 
-    private function stripPrefixes (array $items): array
+    private static function stripPrefixes (array $items): array
     {
         return array_map(function (string $item) {
             return preg_replace('/^[a-zA-Z]+:/', '', $item);
