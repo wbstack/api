010159714e6997ee10f1e26823d88444dcbb9f5e
Frederik Ring <frederik.ring@gmail.com>
2023-12-13 13:29:04 +0100

    Add command for rebuilding QueryService data (#704)
    * feat(qs): scaffold command for rebuilding queryservice data
    
    * feat: add stub methods
    
    * feat: fetch entity ids from mediawiki api
    
    * chore: log received args in dummy job for testing
    
    * refactor: use max page size
    
    * fix: use proper command name
    
    * fix: directly query namespaces table
    
    * fix: do not fail on null lexemes setting
    
    * test: add test setup for qs rebuild command
    
    * test: add test for full command run
    
    * refactor: make sparql url configurable
    
    * test: explicitly set config values in test
    
    * refactor: source command config from cli args with defaults
    
    * test: add test case for command failure
    
    * test: empty wikis shall not dispatch jobs
    
    * feat: allow passing multiple domains
    
    * fix: explicitly request json responses from api
    
    * feat(k8s): add job that can spawn one off queryservice-updater (#701)
    
    * feat(k8s): add job that can spawn one off queryservice-updater
    
    * fix: consider entity payload for job uniqueness
    
    * refactor: read env vars through config
    
    * chore: dispatch proper job
    
    * fix: kubernetes labels cannot contain commas
    
    * docs: add changelog
    
    * chore: pass exception to extra logging args
    
    * chore: add logging to command
    
    * fix: entity payload in labels exceeds allowed length

---
+++ app/Console/Commands/RebuildQueryserviceData.php
@@ -0,0 +1,163 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
+use App\Wiki;
+use App\WikiSetting;
+use App\QueryserviceNamespace;
+use App\Jobs\SpawnQueryserviceUpdaterJob;
+
+class RebuildQueryserviceData extends Command
+{
+    private const NAMESPACE_ITEM = 120;
+    private const NAMESPACE_PROPERTY = 122;
+    private const NAMESPACE_LEXEME = 146;
+
+    protected $signature = 'wbs-qs:rebuild {--domain=*} {--chunkSize=50} {--sparqlUrlFormat=http://queryservice.default.svc.cluster.local:9999/bigdata/namespace/%s/sparql}';
+
+    protected $description = 'Rebuild the queryservice data for a certain wiki or all wikis';
+
+    protected int $chunkSize;
+    protected string $apiUrl;
+    protected string $sparqlUrlFormat;
+
+    public function handle()
+    {
+        $this->chunkSize = intval($this->option('chunkSize'));
+        $this->sparqlUrlFormat = $this->option('sparqlUrlFormat');
+        $this->apiUrl = getenv('PLATFORM_MW_BACKEND_HOST').'/w/api.php';
+
+        $wikiDomains = $this->option('domain');
+        $exitCode = 0;
+
+        $wikis = count($wikiDomains) !== 0
+            ? Wiki::whereIn('domain', $wikiDomains)->get()
+            : Wiki::query()->get();
+
+        $jobTotal = 0;
+        $skippedWikis = 0;
+        $processedWikis = 0;
+        foreach ($wikis as $wiki) {
+            try {
+                $entities = $this->getEntitiesForWiki($wiki);
+                $sparqlUrl = $this->getSparqlUrl($wiki);
+            } catch (\Exception $ex) {
+                Log::error(
+                    'Failed to get prerequisites for enqueuing wiki '.$wiki->domain.', will not dispatch jobs.',
+                    [$ex],
+                );
+                $exitCode = 1;
+                $skippedWikis += 1;
+                break;
+            }
+
+            $entityChunks = array_chunk($entities, $this->chunkSize);
+            foreach ($entityChunks as $entityChunk) {
+                dispatch(
+                    new SpawnQueryserviceUpdaterJob(
+                        $wiki->domain,
+                        implode(',', $entityChunk),
+                        $sparqlUrl,
+                    )
+                );
+            }
+            $jobTotal += count($entityChunks);
+            $processedWikis += 1;
+            Log::info('Dispatched '.count($entityChunks).' job(s) for wiki '.$wiki->domain.'.');
+        }
+
+        Log::info(
+            'Done. Jobs dispatched: '.$jobTotal.' Wikis processed: '.$processedWikis.' Wikis skipped: '.$skippedWikis
+        );
+        return $exitCode;
+    }
+
+    private function getEntitiesForWiki (Wiki $wiki): array
+    {
+        $items = $this->fetchPagesInNamespace($wiki->domain, self::NAMESPACE_ITEM);
+        $properties = $this->fetchPagesInNamespace($wiki->domain, self::NAMESPACE_PROPERTY);
+
+        $lexemesSetting = WikiSetting::where(
+            [
+                'wiki_id' => $wiki->id,
+                'name' => WikiSetting::wwExtEnableWikibaseLexeme
+            ],
+        )->first();
+        $hasLexemesEnabled = $lexemesSetting !== null && $lexemesSetting->value === '1';
+        $lexemes = $hasLexemesEnabled
+            ? $this->fetchPagesInNamespace($wiki->domain, self::NAMESPACE_LEXEME)
+            : [];
+
+        $merged = array_merge($items, $properties, $lexemes);
+        return $this->stripPrefixes($merged);
+    }
+
+    private function getSparqlUrl (Wiki $wiki): string
+    {
+        $match = QueryserviceNamespace::where(['wiki_id' => $wiki->id])->first();
+        if (!$match) {
+            throw new \Exception(
+                'Unable to find queryservice namespace record for wiki '.$wiki->domain
+            );
+        }
+        return sprintf($this->sparqlUrlFormat, $match->namespace);
+    }
+
+    private function fetchPagesInNamespace(string $wikiDomain, int $namespace): array
+    {
+        $titles = [];
+        $cursor = '';
+        while (true) {
+            $response = Http::withHeaders([
+                'host' => $wikiDomain
+            ])->get(
+                $this->apiUrl,
+                [
+                    'action' => 'query',
+                    'list' => 'allpages',
+                    'apnamespace' => $namespace,
+                    'apcontinue' => $cursor,
+                    'aplimit' => 'max',
+                    'format' => 'json',
+                ],
+            );
+
+            if ($response->failed()) {
+                throw new \Exception(
+                    'Failed to fetch allpages for wiki '.$wikiDomain
+                );
+            }
+
+            $jsonResponse = $response->json();
+            $error = data_get($jsonResponse, 'error');
+            if ($error !== null) {
+                throw new \Exception(
+                    'Error response fetching allpages for wiki '.$wikiDomain.': '.$error
+                );
+            }
+
+            $pages = data_get($jsonResponse, 'query.allpages', []);
+            $titles = array_merge($titles, array_map(function (array $page) {
+                return $page['title'];
+            }, $pages));
+
+            $nextCursor = data_get($jsonResponse, 'continue.apcontinue');
+            if ($nextCursor === null) {
+                break;
+            }
+            $cursor = $nextCursor;
+        }
+
+        return $titles;
+    }
+
+    private static function stripPrefixes (array $items): array
+    {
+        return array_map(function (string $item) {
+            return preg_replace('/^[a-zA-Z]+:/', '', $item);
+        }, $items);
+    }
+}
--- app/QueryserviceNamespace.php
+++ app/QueryserviceNamespace.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 /**
  * App\QueryserviceNamespace.
@@ -27,6 +28,7 @@ use Illuminate\Database\Eloquent\Model;
  */
 class QueryserviceNamespace extends Model
 {
+    use HasFactory;
     /**
      * The attributes that are mass assignable.
      *
