b2b5b6a2cfbb34e8d22a95de0ffaa2c47b979428
Deniz Erdogan <91744937+deer-wmde@users.noreply.github.com>
2024-02-08 19:15:28 +0100

    add FailJob (#738)

---
+++ app/Jobs/FailJob.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Jobs;
+
+class FailJob extends Job
+{
+	public function handle(): void
+	{
+		$this->fail(self::class.': The job failed successfully (this is intended, as this is a test Job). Have a nice day.');
+	}
+}
+
