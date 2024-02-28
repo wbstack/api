fba0b4b4bf06aa97e23d435f3298ef276aa3a5ac
Frederik Ring <frederik.ring@wikimedia.de>
2024-02-28 13:44:11 +0100

    feat(queue): manage queue using laravel/horizon package

--- start.sh
+++ start.sh
@@ -4,7 +4,6 @@ set -e
 
 role=${CONTAINER_ROLE:-app}
 env=${APP_ENV:-production}
-queue_name=${QUEUE_NAME:-default}
 
 # if [ "$env" != "local" ]; then
 #     echo "Caching configuration..."
@@ -17,8 +16,7 @@ if [ "$role" = "app" ]; then
 
 elif [ "$role" = "queue" ]; then
 
-    echo "Running the $queue_name queue..."
-    php /var/www/html/artisan queue:work --verbose --tries=5 --timeout=90 --queue="$queue_name"
+    php /var/www/html/artisan horizon
 
 elif [ "$role" = "scheduler" ]; then
 
