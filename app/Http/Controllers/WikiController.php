0fe75bcf894c8e6345dccf1d6d31442c92678f08
Perside Rosalie <perside.rosalie@wikimedia.de>
2024-07-02 11:39:39 +0200

    Save reason for deleting wikis (#825)
    * Save reason for deleting wikis
    
    * Adjust test to check contents of database
    
    Co-authored-by: Perside Rosalie <perside.rosalie@wikimedia.de>
    
    ---------
    
    Co-authored-by: Thomas Arrow <thomas.arrow@wikimedia.de>

--- app/Http/Controllers/WikiController.php
+++ app/Http/Controllers/WikiController.php
@@ -44,7 +44,7 @@ class WikiController extends Controller
         }
 
         $user = $request->user();
-        
+
         $submittedDomain = strtolower($request->input('domain'));
         $submittedDomain = DomainHelper::encode($submittedDomain);
 
@@ -141,7 +141,7 @@ class WikiController extends Controller
                 dispatch(new ElasticSearchAliasInit($wiki->id));
             }
         }
-        
+
         // opportunistic dispatching of jobs to make sure storage pools are topped up
         dispatch(new ProvisionWikiDbJob(null, null, 10));
         dispatch(new ProvisionQueryserviceNamespaceJob(null, 10));
@@ -167,18 +167,24 @@ class WikiController extends Controller
 
         $wikiId = $request->input('wiki');
         $userId = $user->id;
+        $wikiDeletionReason = $request->input('deletionReason');
 
         // Check that the requesting user manages the wiki
         if (WikiManager::where('user_id', $userId)->where('wiki_id', $wikiId)->count() !== 1) {
             // The deletion was requested by a user that does not manage the wiki
             return response()->json('Unauthorized', 401);
         }
+        $wiki = Wiki::find($wikiId);
 
+        if(isset($wikiDeletionReason)){
+            //Save the wiki deletion reason
+            $wiki->update(['wiki_deletion_reason' => $wikiDeletionReason]);
+        }
         // Delete the wiki
-        Wiki::find($wikiId)->delete();
+        $wiki->delete();
 
         // Return a success
-        return response()->json('Success', 200);
+        return response()->json("Success", 200);
     }
 
     // TODO should this just be get wiki?
