1948fa89b8f1fa44a08a1f58596ae33f1f421d6d
Frederik Ring <frederik.ring@wikimedia.de>
2024-07-03 11:25:36 +0200

    feat: fetch credentials from mediawiki

--- app/Jobs/WikiEntityImportJob.php
+++ app/Jobs/WikiEntityImportJob.php
@@ -8,6 +8,7 @@ use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Http;
 use App\Wiki;
 
 class WikiEntityImportJob implements ShouldQueue
@@ -36,12 +37,27 @@ class WikiEntityImportJob implements ShouldQueue
 
     private static function acquireCredentials(string $wikiDomain): OAuthCredentials
     {
-        return new OAuthCredentials(
-            consumerToken: '?',
-            consumerSecret: '?',
-            accessToken: '?',
-            accessSecret: '?',
+        $response = Http::withHeaders(['host' => $wikiDomain])->asForm()->post(
+            getenv('PLATFORM_MW_BACKEND_HOST').'/w/api.php?action=wbstackPlatformOauthGet&format=json',
+            [
+                'consumerName' => 'WikiEntityImportJob',
+                'ownerOnly' => '1',
+                'consumerVersion' => '1',
+                'grants' => 'createeditmovepage|highvolume',
+                'callbackUrlTail' => '/w/index.php',
+            ],
         );
+
+        if ($response->status() > 399) {
+            throw new \Exception('Unexpected status code '.$response->status().' from Mediawiki');
+        }
+
+        $body = $response->json();
+        if (!$body || $body['wbstackPlatformOauthGet']['success'] !== '1') {
+            throw new \ErrorException('Unexpected error acquiring oauth credentials for wiki '.$wikiDomain);
+        }
+
+        return OAuthCredentials::unmarshalMediaWikiResponse($body);
     }
 }
 
@@ -54,6 +70,17 @@ class OAuthCredentials
         public string $accessSecret,
     )
     {}
+
+    public static function unmarshalMediaWikiResponse(array $response): OAuthCredentials
+    {
+        $data = $response['wbstackPlatformOauthGet']['data'];
+        return new OAuthCredentials(
+            consumerToken: $data['consumerKey'],
+            consumerSecret: $data['consumerSecret'],
+            accessToken: $data['accessKey'],
+            accessSecret: $data['accessSecret'],
+        );
+    }
 
     public function marshalEnv(string $prefix = 'TARGET_WIKI'): array
     {
