7987e8e65d8fcc750df910678058de31eb5f4877
Frederik Ring <frederik.ring@gmail.com>
2024-07-03 16:09:06 +0200

    Add API endpoints for handling entity imports (#823)
    * feat: scaffold model for storing data about transferbot runs
    
    * feat: scaffold controller logic
    
    * fix: wiki access needs to be scoped down to users
    
    * fix: use consistent plural form for table name
    
    * feat: dispatch mock job on creation
    
    * test: scaffold test setup
    
    * test: cover further functionality
    
    * fix: use proper controller reference
    
    * fix: store entire data
    
    * fix: only update pending imports
    
    * chore: import is not used
    
    * chore: rename dummy job
    
    * feat: add input validation
    
    * refactor: handle enum validation through laravel features

---
+++ tests/Routes/Wiki/EntityImportBackendTest.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Tests\Routes\Wiki\Managers;
+
+use App\WikiEntityImportStatus;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+use Tests\TestCase;
+use App\Wiki;
+use App\WikiEntityImport;
+use App\WikiManager;
+
+class EntityImportBackendTest extends TestCase
+{
+    protected $route = 'backend/wiki/updateEntityImport';
+
+    use RefreshDatabase;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        Wiki::query()->delete();
+        WikiManager::query()->delete();
+        WikiEntityImport::query()->delete();
+    }
+
+    public function tearDown(): void
+    {
+        Wiki::query()->delete();
+        WikiManager::query()->delete();
+        WikiEntityImport::query()->delete();
+        parent::tearDown();
+    }
+
+    public function testNoUpdate()
+    {
+        $this->json('PATCH', $this->route, ['wiki_entity_import' => 789, 'status' => 'success'])
+            ->assertStatus(404);
+    }
+
+    public function testBadStatus()
+    {
+        $wiki = Wiki::factory()->create(['domain' => 'test.wikibase.cloud']);
+        $import = WikiEntityImport::factory()->create([
+            'status' => WikiEntityImportStatus::Pending,
+            'wiki_id' => $wiki->id,
+        ]);
+        $this->json('PATCH', $this->route."?wiki_entity_import=".$import->id, ['status' => 'finished'])
+            ->assertStatus(422);
+    }
+
+    public function testUpdate()
+    {
+        $wiki = Wiki::factory()->create(['domain' => 'test.wikibase.cloud']);
+        $import = WikiEntityImport::factory()->create([
+            'status' => WikiEntityImportStatus::Pending,
+            'wiki_id' => $wiki->id,
+        ]);
+        $this->json('PATCH', $this->route."?wiki_entity_import=".$import->id, ['status' => 'success'])
+            ->assertStatus(200)
+            ->assertJsonPath('data.status', 'success');
+    }
+
+    public function testUpdateWhenDone()
+    {
+        $wiki = Wiki::factory()->create(['domain' => 'test.wikibase.cloud']);
+        $import = WikiEntityImport::factory()->create([
+            'status' => WikiEntityImportStatus::Failed,
+            'wiki_id' => $wiki->id,
+        ]);
+        $this->json('PATCH', $this->route."?wiki_entity_import=".$import->id, ['status' => 'success'])
+            ->assertStatus(400);
+        $this->assertEquals(
+            WikiEntityImportStatus::Failed,
+            WikiEntityImport::find($import->id)->status,
+        );
+    }
+}
