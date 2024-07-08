6c7424bf068fcf074f751da34b9fa8c9758907a4
Frederik Ring <frederik.ring@gmail.com>
2024-07-08 14:03:57 +0200

    feat: add job for marking stalled entity imports as stalled (#833)

---
+++ app/Jobs/FailStalledEntityImportsJob.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Jobs;
+
+use App\WikiEntityImportStatus;
+use Carbon\Carbon;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
+use App\WikiEntityImport;
+
+class FailStalledEntityImportsJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+    public function handle(): void
+    {
+        $deadline = Carbon::now()->subHours(24);
+        $now = Carbon::now();
+
+        $stalledImports = WikiEntityImport::where([
+            ['status', '=', WikiEntityImportStatus::Pending],
+            ['started_at',  '<=', $deadline],
+        ]);
+        $stalledImports->update([
+            'status' => WikiEntityImportStatus::Failed,
+            'finished_at' => $now,
+        ]);
+
+        if ($stalledImports->count() > 0) {
+            Log::info(
+                "Marked ".$stalledImports->count()." WikiEntityImports as failed as they seem to be stalled."
+            );
+        }
+    }
+}
