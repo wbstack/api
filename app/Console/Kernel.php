7b1f1dec2481d2503d88966cbbc25de43df2dda2
Perside Rosalie <perside.rosalie@wikimedia.de>
2025-02-10 14:28:27 +0100

    Add table and job for daily tracking and recording of wiki metrics (#880)
    * Add WikiDailyMetric table
    Add Job for the above table updates daily
    
    Bug:T383421
    
    * import needed classes in test
    
    * Refactoring: created a wiki class in the metric folde and added tests for it
    
    * added comments
    
    * Remove test annotations
    
    * Update CHANGELOG
    
    * Fix typo in CHANGELOG and tests

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -10,11 +10,13 @@ use App\Jobs\PruneQueryserviceBatchesTable;
 use App\Jobs\RequeuePendingQsBatchesJob;
 use App\Jobs\SandboxCleanupJob;
 use App\Jobs\PollForMediaWikiJobsJob;
+use App\Jobs\UpdateWikiDailyMetricJob;
 use App\Jobs\UpdateWikiSiteStatsJob;
 use App\Jobs\SendEmptyWikiNotificationsJob;
 use App\Jobs\CreateQueryserviceBatchesJob;
 use App\Jobs\FailStalledEntityImportsJob;
 use App\Jobs\UpdateQueryserviceAllowList;
+use App\Wiki;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -59,7 +61,10 @@ class Kernel extends ConsoleKernel
 
         $schedule->job(new SendEmptyWikiNotificationsJob)->dailyAt('21:00');
 
+        $schedule->job(new UpdateWikiDailyMetricJob)->dailyAt('23:00');
+
         $schedule->job(new UpdateQueryserviceAllowList)->weeklyOn(Schedule::MONDAY, '01:00');
+
     }
 
     /**
---
+++ app/Metrics/App/WikiMetrics.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Metrics\App;
+
+use App\Wiki;
+use App\WikiDailyMetrics;
+
+class WikiMetrics
+{
+
+    public function saveMetrics(Wiki $wiki): void
+    {
+        $today = now()->format('Y-m-d');
+        $oldRecord = WikiDailyMetrics::where('wiki_id', $wiki->id)->latest('date')->first();
+        $todayPageCount = $wiki->wikiSiteStats()->first()->pages ?? 0;
+        $isDeleted = (bool)$wiki->deleted_at;
+        if ($oldRecord) {
+            if ($oldRecord->is_deleted) {
+                \Log::info("WikiMetrics is deleted, no new record for WikiMetrics ID {$wiki->id}.");
+                return;
+            }
+            if ($oldRecord->pages === $todayPageCount) {
+                \Log::info("Page count unchanged for WikiMetrics ID {$wiki->id}, no new record added.");
+                return;
+            }
+        }
+        WikiDailyMetrics::create([
+            'id' => $wiki->id . '_' . date('Y-m-d'),
+            'pages' => $todayPageCount,
+            'is_deleted' => $isDeleted,
+            'date' => $today,
+            'wiki_id' => $wiki->id,
+        ]);
+        \Log::info("New metric recorded for WikiMetrics ID {$wiki->id}");
+    }
+
+}
+
