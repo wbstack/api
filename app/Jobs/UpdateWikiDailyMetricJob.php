61fc43f05701364c73698fc5579c2a70f8b70659
Perside Rosalie <perside.rosalie@wikimedia.de>
2025-02-18 12:05:43 +0100

    Fix Job definition

--- app/Jobs/UpdateWikiDailyMetricJob.php
+++ app/Jobs/UpdateWikiDailyMetricJob.php
@@ -4,15 +4,16 @@ namespace App\Jobs;
 
 use App\Wiki;
 use \App\Metrics\App\WikiMetrics;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 
 //This job is for the daily measurements of metrics per wikibases.
 //This is to help in understanding the purpose of active wikis.
-class UpdateWikiDailyMetricJob implements ShouldQueue
+class UpdateWikiDailyMetricJob extends Job implements ShouldBeUnique
 {
     use Dispatchable;
-    public int $timeout = 3600;
+    public $timeout = 3600;
 
     /**
      * Execute the job.
