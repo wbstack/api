32adc37fc6155dfb186c0efeb111a70d0c2fa7a2
Thomas Arrow <tarrow@users.noreply.github.com>
2025-03-24 17:32:01 +0000

    Allow and require audience in profile if data-hub (#906)
    * only allow audience in profile if data-hub
    
    * fix and improve tests

--- app/Helper/ProfileValidator.php
+++ app/Helper/ProfileValidator.php
@@ -11,7 +11,7 @@ class ProfileValidator
         return Validator::make($profile, [
             'purpose' => 'in:data_hub,data_lab,tool_lab,test_drive,decide_later,other',
             'purpose_other' => 'string|required_if:purpose,other|missing_unless:purpose,other',
-            'audience' => 'in:narrow,wide,other',
+            'audience' => 'in:narrow,wide,other|required_if:purpose,data_hub|missing_unless:purpose,data_hub',
             'audience_other' => 'string|required_if:audience,other|missing_unless:audience,other',
             'temporality' => 'in:permanent,temporary,decide_later,other',
             'temporality_other' => 'string|required_if:temporality,other|missing_unless:temporality,other',
