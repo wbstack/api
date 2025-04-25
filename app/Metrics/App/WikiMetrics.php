c83671206b50f6d4fd8b68f7cebbe39724a10074
dena <dena@wikimedia.de>
2025-04-25 13:06:59 +0200

    use areMetricsEqual method

--- app/Metrics/App/WikiMetrics.php
+++ app/Metrics/App/WikiMetrics.php
@@ -7,32 +7,37 @@ use App\WikiDailyMetrics;
 
 class WikiMetrics
 {
-
     public function saveMetrics(Wiki $wiki): void
     {
         $today = now()->format('Y-m-d');
         $oldRecord = WikiDailyMetrics::where('wiki_id', $wiki->id)->latest('date')->first();
         $todayPageCount = $wiki->wikiSiteStats()->first()->pages ?? 0;
         $isDeleted = (bool)$wiki->deleted_at;
+
+        $dailyMetrics = new WikiDailyMetrics([
+            'id' => $wiki->id . '_' . date('Y-m-d'),
+            'pages' => $todayPageCount,
+            'is_deleted' => $isDeleted,
+            'date' => $today,
+            'wiki_id' => $wiki->id
+        ]);
+        
         if ($oldRecord) {
             if ($oldRecord->is_deleted) {
                 \Log::info("Wiki is deleted, no new record for WikiMetrics ID {$wiki->id}.");
                 return;
             }
+
             if (!$isDeleted) {
-                if ($oldRecord->pages === $todayPageCount) {
-                    \Log::info("Page count unchanged for WikiMetrics ID {$wiki->id}, no new record added.");
+                if ($oldRecord->areMetricsEqual($dailyMetrics)) {
+                    \Log::info("Record unchanged for WikiMetrics ID {$wiki->id}, no new record added.");
                     return;
                 }
             }
         }
-        WikiDailyMetrics::create([
-            'id' => $wiki->id . '_' . date('Y-m-d'),
-            'pages' => $todayPageCount,
-            'is_deleted' => $isDeleted,
-            'date' => $today,
-            'wiki_id' => $wiki->id,
-        ]);
+
+        $dailyMetrics->save();
+
         \Log::info("New metric recorded for WikiMetrics ID {$wiki->id}");
     }
 
