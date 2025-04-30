3795dd00e9a0b13f4de7472b1e4fd936dfb327e8
Andrew Kostka <andrew.kostka@wikimedia.de>
2025-04-30 14:32:22 +0000

    Add POST endpoint for Wiki profiles

---
+++ app/Http/Controllers/WikiProfileController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Helper\ProfileValidator;
+use App\Wiki;
+use App\WikiProfile;
+use Illuminate\Http\Request;
+
+class WikiProfileController extends Controller
+{
+    private $profileValidator;
+
+    public function __construct(ProfileValidator $profileValidator)
+    {
+        $this->profileValidator = $profileValidator;
+    }
+
+    public function create(Request $request): \Illuminate\Http\JsonResponse
+    {
+        $validatedInput = $request->validate([
+            'wiki' => ['required', 'integer'],
+            'profile' => ['required', 'json']
+        ]);
+
+        $wiki = Wiki::find($validatedInput['wiki']);
+        if (!$wiki) {
+            abort(404, 'No such wiki');
+        }
+
+        $rawProfile = json_decode($validatedInput['profile'], true);
+        $profileValidator = $this->profileValidator->validate($rawProfile);
+        $profileValidator->validateWithBag('post');
+
+        $profile = WikiProfile::create(['wiki_id' => $wiki->id, ...$rawProfile]);
+        return response()->json(['data' => $profile]);
+    }
+}
