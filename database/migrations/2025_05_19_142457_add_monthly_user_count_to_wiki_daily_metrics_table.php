fe228562e569fb9c4e5099ebeeaeca07716cc9c8
Perside Rosalie <perside.rosalie@wikimedia.de>
2025-05-20 09:07:03 +0200

    add DB migration

---
+++ database/migrations/2025_05_19_142457_add_monthly_user_count_to_wiki_daily_metrics_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('wiki_daily_metrics', function (Blueprint $table) {
+            $table->integer('monthly_casual_users')->nullable();
+            $table->integer('monthly_active_users')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('wiki_daily_metric', function (Blueprint $table) {
+        });
+    }
+};
