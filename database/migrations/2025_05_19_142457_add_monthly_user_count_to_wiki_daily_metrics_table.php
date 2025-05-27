9a7377ccbc1f56c7bb98de924c94c62a2536640a
Perside Rosalie <perside.rosalie@wikimedia.de>
2025-05-27 12:25:37 +0200

    Add metrics: monthly casual users and active users (#936)
    * add DB migration
    
    * Add metrics monthly casual users and active users
    
    * add new metrics to WikiDailyMetrics::$metricNames as well
    
    * fix migration reversal code
    
    * update changelog
    
    ---------
    
    Co-authored-by: dena <dena@wikimedia.de>

---
+++ database/migrations/2025_05_19_142457_add_monthly_user_count_to_wiki_daily_metrics_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('wiki_daily_metrics', function (Blueprint $table) {
+            $table->integer('monthly_casual_users')->nullable();
+            $table->integer('monthly_active_users')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('wiki_daily_metrics', function (Blueprint $table) {
+            $table->dropColumn('monthly_casual_users');
+            $table->dropColumn('monthly_active_users');
+        });
+    }
+};
