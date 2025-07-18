ff9704d157f86cc957c50ce8430b4b9916d15fb3
dena <dena@wikimedia.de>
2025-07-18 13:57:57 +0200

    extend test cases

--- tests/Routes/Complaint/SendMessageTest.php
+++ tests/Routes/Complaint/SendMessageTest.php
@@ -2,15 +2,19 @@
 
 namespace Tests\Routes\Complaint;
 
+use App\ComplaintRecord;
 use App\Notifications\ComplaintNotification;
 use App\Rules\ReCaptchaValidation;
 use Illuminate\Notifications\AnonymousNotifiable;
 use Illuminate\Support\Facades\Notification;
 use Tests\TestCase;
 use Tests\Feature\ReCaptchaValidationTest;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class SendMessageTest extends TestCase
 {
+    use RefreshDatabase;
+    
     protected $route = 'complaint/sendMessage';
 
     protected $postDataTemplateEmpty = [
@@ -38,71 +42,141 @@ class SendMessageTest extends TestCase
     
         $this->app->instance(ReCaptchaValidation::class, $mockRule);
     }
+    private function assertRecordCount(int $count)
+    {
+        $this->assertEquals(ComplaintRecord::count(), $count);
+    }
+
+    private function assertComplaintMarkedAsDispatched()
+    {
+        $complaintRecord = ComplaintRecord::first();
+        $this->assertNotEmpty($complaintRecord->dispatched_at);
+    }
+
+    private function assertComplaintNotMarkedAsDispatched()
+    {
+        $complaintRecord = ComplaintRecord::first();
+        $this->assertEmpty($complaintRecord->dispatched_at);
+    }
+
+    private function assertComplaintRecorded()
+    {
+        $this->assertRecordCount(1);
+    }
+
+    private function assertComplaintNotRecorded()
+    {
+        $this->assertRecordCount(0);
+    }
+
+    public function testRecordOnMailFail()
+    {
+        $this->mockReCaptchaValidation();
+
+        $data = $this->postDataTemplateFilled;
+
+        try {
+            $response = $this->json('POST', $this->route, $data);
+        } catch(\Symfony\Component\Mailer\Exception\TransportException $e) {
+            return;
+        }
+
+        $response->assertStatus(500);
+
+        $this->assertComplaintRecorded();
+        $this->assertComplaintNotMarkedAsDispatched();
+    }
 
     public function testSendMessage_NoData()
     {
+        Notification::fake();
         $this->mockReCaptchaValidation(false);
 
         $data = $this->postDataTemplateEmpty;
 
         $response = $this->json('POST', $this->route, $data);
         $response->assertStatus(401);
+
+        Notification::assertNothingSent();
+        $this->assertComplaintNotRecorded();
     }
 
     public function testSendMessage_InvalidMailAddressRfc()
     {
+        Notification::fake();
         $this->mockReCaptchaValidation();
 
         $data = $this->postDataTemplateFilled;
         $data['mailAddress'] = "invalid-mail-address";
         $response = $this->json('POST', $this->route, $data);
         $response->assertStatus(400);
+
+        $this->assertEquals(ComplaintRecord::count(), 0);
+
+        Notification::assertNothingSent();
+        $this->assertComplaintNotRecorded();
     }
 
     public function testSendMessage_InvalidMailAddressMulti()
     {
+        Notification::fake();
         $this->mockReCaptchaValidation();
 
         $data = $this->postDataTemplateFilled;
         $data['mailAddress'] = "mail@example.com, foo@bar.com";
         $response = $this->json('POST', $this->route, $data);
         $response->assertStatus(400);
+
+        Notification::assertNothingSent();
+        $this->assertComplaintNotRecorded();
     }
 
     public function testSendMessage_ReasonTooLong()
     {
+        Notification::fake();
         $this->mockReCaptchaValidation();
 
         $data = $this->postDataTemplateFilled;
         $data['reason'] = str_repeat("Hi!", 10000);
         $response = $this->json('POST', $this->route, $data);
         $response->assertStatus(400);
+
+        Notification::assertNothingSent();
+        $this->assertComplaintNotRecorded();
     }
 
     public function testSendMessage_NameTooLong()
     {
+        Notification::fake();
         $this->mockReCaptchaValidation();
 
         $data = $this->postDataTemplateFilled;
         $data['name'] = str_repeat("Hi!", 10000);
         $response = $this->json('POST', $this->route, $data);
         $response->assertStatus(400);
+
+        Notification::assertNothingSent();
+        $this->assertComplaintNotRecorded();
     }
 
     public function testSendMessage_OffendingUrlsTooLong()
     {
+        Notification::fake();
         $this->mockReCaptchaValidation();
 
         $data = $this->postDataTemplateFilled;
         $data['offendingUrls'] = str_repeat("Hi!", 10000);
         $response = $this->json('POST', $this->route, $data);
         $response->assertStatus(400);
+
+        Notification::assertNothingSent();
+        $this->assertComplaintNotRecorded();
     }
 
     public function testSendMessage_NoNameNorMailAddress()
     {
-        $this->mockReCaptchaValidation();
         Notification::fake();
+        $this->mockReCaptchaValidation();
 
         $data = $this->postDataTemplateFilled;
         $data['name'] = '';
@@ -110,12 +184,16 @@ class SendMessageTest extends TestCase
 
         $response = $this->json('POST', $this->route, $data);
         $response->assertStatus(200);
+
+        Notification::assertCount(1);
+        $this->assertComplaintRecorded();
+        $this->assertComplaintMarkedAsDispatched();
     }
 
     public function testSendMessage_Success()
     {
-        $this->mockReCaptchaValidation();
         Notification::fake();
+        $this->mockReCaptchaValidation();
 
         $data = $this->postDataTemplateFilled;
 
@@ -128,16 +206,21 @@ class SendMessageTest extends TestCase
             );
             return true;
         });
+
+        Notification::assertCount(2);
+        $this->assertComplaintRecorded();
+        $this->assertComplaintMarkedAsDispatched();
     }
 
     public function testSendMessage_RecaptchaFailure()
     {
-        $this->mockReCaptchaValidation(false);
         Notification::fake();
+        $this->mockReCaptchaValidation(false);
 
         $response = $this->json('POST', $this->route, $this->postDataTemplateFilled);
         $response->assertStatus(401);
 
         Notification::assertNothingSent();
+        $this->assertComplaintNotRecorded();
     }
 }
