3c8924ceccfec99b02bdd502948c48b655ac0cb5
Dat <dat.nguyen@wikimedia.de>
2025-09-29 23:55:15 +0200

    add lastest() method to get lastest version of ToU

--- app/TermOfUseVersion.php
+++ app/TermOfUseVersion.php
@@ -6,4 +6,19 @@ enum TermOfUseVersion: string {
     case v0 = '2025-08-21';
     // case V1 = 'yyyy-mm-dd';
     // case V2 = 'yyyy-mm-dd';
+    public static function latest(): self {
+        $latestVersion = self::v0;
+        $latestNum = 0;
+        foreach (self::cases() as $case) {
+            if (!str_starts_with($case->name, 'v')) {
+                continue;
+            }
+            $n = (int) substr($case->name, 1);
+            if ($n > $latestNum) {
+                $latestNum = $n;
+                $latestVersion = $case;
+            }
+        }
+        return $latestVersion;
+    }
 }
