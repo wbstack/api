89e06f44b7c1e1059b5072c130e803bcb63bda37
dena <91744937+deer-wmde@users.noreply.github.com>
2025-11-06 13:34:20 +0100

    adds wbs-user:disable command (#992)
    * adds wbs-user:disable command
    
    * linting
    
    * cleanup
    
    * add unit test
    
    * linting fix

---
+++ tests/Commands/User/DisableTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Tests\Commands;
+
+use App\User;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Tests\TestCase;
+
+class DisableTest extends TestCase {
+    use DatabaseTransactions;
+
+    const EMAIL = 'mail@example.com';
+
+    private function createUser($email) {
+        $user = new User([
+            'email' => $email,
+            'password' => 'worldsstrongestpassword',
+        ]);
+        $user->save();
+
+        return $user;
+    }
+
+    public function testSuccess() {
+        $oldUser = $this->createUser(self::EMAIL);
+        $oldUserId = $oldUser->id;
+
+        $this->artisan('wbs-user:disable',
+            [
+                '--email' => self::EMAIL,
+            ]
+        )->assertExitCode(0);
+
+        $newUser = User::firstWhere('id', $oldUserId);
+
+        $this->assertSame($oldUser->id, $newUser->id);
+        $this->assertSame($newUser->email, '');
+        $this->assertFalse($newUser->hasVerifiedEmail());
+    }
+
+    public function testUserNotFound() {
+        $this->artisan('wbs-user:disable',
+            [
+                '--email' => self::EMAIL,
+            ]
+        )->assertExitCode(2);
+    }
+}
