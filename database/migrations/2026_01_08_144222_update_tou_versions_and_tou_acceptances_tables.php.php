8c54b91404d371a45073ded841b8ef5f150d7b75
Dat <dat.nguyen@wikimedia.de>
2026-01-08 15:52:22 +0100

    Fix linting errors

--- database/migrations/2026_01_08_144222_update_tou_versions_and_tou_acceptances_tables.php.php
+++ database/migrations/2026_01_08_144222_update_tou_versions_and_tou_acceptances_tables.php.php
@@ -4,13 +4,11 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
+    public function up(): void {
         Schema::table('tou_versions', function (Blueprint $table) {
             $table->dropColumn('id');
             $table->char('version', 10)->primary()->change();
@@ -29,8 +27,7 @@ return new class extends Migration
     /**
      * Reverse the migrations.
      */
-    public function down(): void
-    {
+    public function down(): void {
         Schema::dropIfExists('tou_versions');
         Schema::dropIfExists('tou_acceptances');
     }
