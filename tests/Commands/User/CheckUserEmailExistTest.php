585e740f2b071a3e359231c9b91feb47126d536c
Perside Rosalie <perside.rosalie@wikimedia.de>
2026-01-08 22:00:01 +0100

    Add more test

--- tests/Commands/User/CheckUserEmailExistTest.php
+++ tests/Commands/User/CheckUserEmailExistTest.php
@@ -6,7 +6,7 @@ use App\User;
 use Tests\TestCase;
 
 class CheckUserEmailExistTest extends TestCase {
-    public function testItFindsEmailInApidbUsersTable() {
+    public function testItFindsEmailInApiUsersTable() {
         User::factory()->create([
             'email' => 'user@example.com',
         ]);
@@ -20,6 +20,17 @@ class CheckUserEmailExistTest extends TestCase {
     public function testItReturnsNotFoundIfEmailDoesNotExist() {
         $this->artisan('wbs-user:check-email', ['emails' => ['nonexistent@example.com']])
             ->expectsOutput('NOT FOUND: nonexistent@example.com')
+            ->assertExitCode(0);
+    }
+
+    public function testItChecksMultipleEmails() {
+        User::factory()->create(['email' => 'user1@example.com']);
+
+        $emails = ['user1@example.com', 'other@example.com'];
+
+        $this->artisan('wbs-user:check-email', ['emails' => $emails])
+            ->expectsOutput('FOUND: user1@example.com in apidb.users')
+            ->expectsOutput('NOT FOUND: other@example.com')
             ->assertExitCode(0);
     }
 }
