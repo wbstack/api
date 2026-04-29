06ea0775761f623ca885d76b5325ddfd20c56e19
Dat <dat.nguyen@wikimedia.de>
2026-04-29 11:59:29 +0200

    fix linting errors

--- database/migrations/2016_06_01_000001_create_oauth_auth_codes_table.php
+++ database/migrations/2016_06_01_000001_create_oauth_auth_codes_table.php
@@ -4,13 +4,11 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
+    public function up(): void {
         Schema::create('oauth_auth_codes', function (Blueprint $table) {
             $table->string('id', 100)->primary();
             $table->unsignedBigInteger('user_id')->index();
@@ -24,8 +22,7 @@ return new class extends Migration
     /**
      * Reverse the migrations.
      */
-    public function down(): void
-    {
+    public function down(): void {
         Schema::dropIfExists('oauth_auth_codes');
     }
 };
--- database/migrations/2016_06_01_000003_create_oauth_refresh_tokens_table.php
+++ database/migrations/2016_06_01_000003_create_oauth_refresh_tokens_table.php
@@ -4,13 +4,11 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
+    public function up(): void {
         Schema::create('oauth_refresh_tokens', function (Blueprint $table) {
             $table->string('id', 100)->primary();
             $table->string('access_token_id', 100)->index();
@@ -22,8 +20,7 @@ return new class extends Migration
     /**
      * Reverse the migrations.
      */
-    public function down(): void
-    {
+    public function down(): void {
         Schema::dropIfExists('oauth_refresh_tokens');
     }
 };
