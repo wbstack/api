97c16037c5569fd815b0155f6714605e8957bc09
Dat <dat.nguyen@wikimedia.de>
2026-06-26 12:43:34 +0200

    Correct method calls and proper mocking with Http:fake()

--- tests/Jobs/MediawikiInitTest.php
+++ tests/Jobs/MediawikiInitTest.php
@@ -24,10 +24,10 @@ class MediawikiInitTest extends TestCase {
         $this->wikiDomain = 'some.domain.com';
         $this->username = 'username';
         $this->email = 'some@email.com';
-        $this->mwBackendHost = 'mediawiki.localhost';
+        $this->mwBackendHost = 'http://mediawiki.localhost';
 
         $this->mockMwHostResolver = $this->createMock(MediaWikiHostResolver::class);
-        $this->mockMwHostResolver->method('getBackendHostForDomain')->willReturn(
+        $this->mockMwHostResolver->method('getBackendUrlForDomain')->willReturn(
             $this->mwBackendHost
         );
     }
