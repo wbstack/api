50322364883f5447f7a90df999287e38acfd5897
Perside Rosalie <perside.rosalie@wikimedia.de>
2026-07-08 09:46:32 +0200

    Remove all terms of use logic code (#1182)
    * Drop database tables
    * Remove Laravel Jobs
    * Remove any logic and tests related to Terms of Use
    
    Bug:  T430532

--- app/Jobs/UserCreateJob.php
+++ app/Jobs/UserCreateJob.php
@@ -2,11 +2,8 @@
 
 namespace App\Jobs;
 
-use App\TermsOfUseVersion;
 use App\User;
-use App\UserTermsOfUseAcceptance;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Log;
 
 class UserCreateJob extends Job {
     private $email;
@@ -33,17 +30,7 @@ class UserCreateJob extends Job {
             'verified' => $this->verified,
         ]);
 
-        $latest = TermsOfUseVersion::latestActiveVersion();
-        if ($latest) {
-            UserTermsOfUseAcceptance::create([
-                'user_id' => $user->id,
-                'tou_version' => $latest->version,
-                'tou_accepted_at' => now(),
-            ]);
-        } else {
-            Log::warning("No active Terms of Use version found when creating user {$user->email} (ID {$user->id}).");
-        }
-
         return $user;
+
     }
 }
