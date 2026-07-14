94fe868651e1d9985e042eddedd62fd2ed883954
Perside Rosalie <perside.rosalie@wikimedia.de>
2026-07-14 13:53:59 +0200

    Removed code that was added accidentally after #1182

--- app/Jobs/UserCreateJob.php
+++ app/Jobs/UserCreateJob.php
@@ -3,10 +3,8 @@
 namespace App\Jobs;
 
 use App\User;
-use App\UserTermsOfUseAcceptance;
 use App\Policy;
 use App\PolicyAcceptance;
-use App\TermsOfUseVersion;
 use Illuminate\Support\Facades\Hash;
 
 class UserCreateJob extends Job {
@@ -15,9 +13,8 @@ class UserCreateJob extends Job {
     private $password;
 
     private $verified;
-    
+
     private $acceptedPolicyIds;
-    
 
     public function __construct($email, $password, $acceptedPolicyIds, $verified = false) {
         // TODO maybe pass in an unsaved eloquent model?
@@ -37,18 +34,6 @@ class UserCreateJob extends Job {
             'password' => Hash::make($this->password),
             'verified' => $this->verified,
         ]);
-
-        // accept latest Terms of Use Policy automatically
-        $latestToU = TermsOfUseVersion::latestActiveVersion();
-        if ($latestToU) {
-            UserTermsOfUseAcceptance::create([
-                'user_id' => $user->id,
-                'tou_version' => $latestToU->version,
-                'tou_accepted_at' => now(),
-            ]);
-        } else {
-            Log::warning("No active Terms of Use version found when creating user {$user->email} (ID {$user->id}).");
-        }
 
         if (is_array($this->acceptedPolicyIds)) {
             foreach($this->acceptedPolicyIds as $acceptedPolicyId) {
