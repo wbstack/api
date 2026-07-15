564103f08fa42320f77018748b6ea34123a53a7c
Thomas Arrow <thomas.arrow@wikimedia.de>
2026-07-15 15:07:37 +0100

    Attempt to match the Response to the ticket spec
    I attempted to do this by:
     - adapting the Collection to not be wrapped in a data object[0]
     - adding the items key to the structure of the ResourceCollection[1]
     - creating a Resource for the structure to represent each Policy as it looks on the wire[2]
     - mapping the existing Collection to use the new PolicyResource
    
    I was led in this direction by a blog[3] and stackoverflow[4]
    
    [0] https://api.laravel.com/docs/11.x/Illuminate/Http/Resources/Json/JsonResource.html#method_wrap
    [1] https://laravel.com/docs/11.x/eloquent-resources#resource-collections
    [2] https://laravel.com/docs/11.x/eloquent-resources#introduction
    [3] https://dev.to/programmerhasan/how-to-use-laravels-api-resource-collections-for-efficient-data-handling-143g
    [4] https://stackoverflow.com/a/67852854

--- app/Http/Resources/PoliciesCollection.php
+++ app/Http/Resources/PoliciesCollection.php
@@ -7,10 +7,14 @@ use Illuminate\Http\Resources\Json\ResourceCollection;
 use Illuminate\Support\Collection;
 
 class PoliciesCollection extends ResourceCollection {
+    public static $wrap = null;
+
     /**
      * Transform the resource collection into an array.
      */
-    public function toArray(Request $request): Collection {
-        return $this->collection;
+    public function toArray(Request $request): array {
+        return ['items' => $this->collection->map(function ($policy) {
+            return new PolicyResource($policy);
+        }), ];
     }
 }
