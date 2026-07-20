8b143b1bda041164df27ba4c70d1a697d8e69b27
Perside Rosalie <perside.rosalie@wikimedia.de>
2026-07-20 12:41:33 +0200

    Create endpoint to get specific policy by type and active_from date
    Bug: T432339

--- tests/Http/Controllers/PoliciesControllerTest.php
+++ tests/Http/Controllers/PoliciesControllerTest.php
@@ -57,4 +57,34 @@ class PoliciesControllerTest extends TestCase {
             'active_from' => $latestActiveHostingPolicy->active_from->format('Y-m-d'),
         ]);
     }
+
+    public function testGetPolicyByTypeAndActiveFrom(): void {
+        Policy::factory()->create([
+            'policy_type' => 'hosting-policy',
+            'active_from' => '2026-07-01',
+        ]);
+
+        Policy::factory()->create([
+            'policy_type' => 'hosting-policy',
+            'active_from' => '2026-07-02',
+        ]);
+
+        $request = $this->getJson('v1/policies/hosting-policy/by_active_from/2026-07-01');
+
+        $request->assertOk();
+        $request->assertJsonFragment([
+            'active_from' => '2026-07-01',
+            'type' => 'hosting-policy',
+        ]);
+    }
+
+    public function testGetPolicyByTypeAndActiveFromReturns422WithInvalidParams(): void {
+        $request = $this->getJson('v1/policies/fake-policy/by_active_from/not-a-date');
+        $request->assertUnprocessable();
+    }
+
+    public function testMissingPolicyByTypeAndActiveFromReturns404(): void {
+        $request = $this->getJson('v1/policies/hosting-policy/by_active_from/2026-07-01');
+        $request->assertNotFound();
+    }
 }
