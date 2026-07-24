9be78941c7fa853a11567f07e1da8be5f9ea9df0
dena <dena@wikimedia.de>
2026-07-24 15:30:09 +0200

    cleanup

--- app/Http/Resources/PolicyResource.php
+++ app/Http/Resources/PolicyResource.php
@@ -15,12 +15,6 @@ class PolicyResource extends JsonResource {
     public static $wrap = null;
 
     public function toArray(Request $request): array {
-        $activeFrom = $this->active_from;
-
-        if ($this->active_from !== null) {
-            $activeFrom = Carbon::parse($this->active_from)->format('Y-m-d');
-        }
-
         return [
             'metadata' => [
                 'policy_id' => $this->id,
