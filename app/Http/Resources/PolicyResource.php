5f2c8cacd395d46fd4780e6812aab332b160b45e
dena <dena@wikimedia.de>
2026-07-24 14:11:36 +0200

    guardrail for null activeFrom

--- app/Http/Resources/PolicyResource.php
+++ app/Http/Resources/PolicyResource.php
@@ -15,11 +15,17 @@ class PolicyResource extends JsonResource {
     public static $wrap = null;
 
     public function toArray(Request $request): array {
+        $activeFrom = $this->active_from;
+
+        if ($this->active_from !== null) {
+            $activeFrom = Carbon::parse($this->active_from)->format('Y-m-d');
+        }
+    
         return [
             'metadata' => [
                 'policy_id' => $this->id,
                 'type' => $this->policy_type,
-                'active_from' => Carbon::parse($this->active_from)->format('Y-m-d'),
+                'active_from' => $activeFrom,
                 'content_vue_file' => $this->content_vue_file,
             ],
         ];
