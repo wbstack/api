7f28ac75aecf42be3effdce2f726a95948cd05a4
dena <91744937+deer-wmde@users.noreply.github.com>
2026-07-24 15:34:29 +0200

    fix PolicyResource: `null` date should not default to valid date (#1215)
    fix for the discovery in https://github.com/wmde/wbaas-deploy/pull/2925
    
    https://phabricator.wikimedia.org/T432714
    
    ---------
    
    Co-authored-by: Dat WMDE <dat.nguyen@wikimedia.de>

--- app/Http/Resources/PolicyResource.php
+++ app/Http/Resources/PolicyResource.php
@@ -19,7 +19,9 @@ class PolicyResource extends JsonResource {
             'metadata' => [
                 'policy_id' => $this->id,
                 'type' => $this->policy_type,
-                'active_from' => Carbon::parse($this->active_from)->format('Y-m-d'),
+                'active_from' => $this->active_from === null
+                    ? null
+                    : Carbon::parse($this->active_from)->format('Y-m-d'),
                 'content_vue_file' => $this->content_vue_file,
             ],
         ];
---
+++ tests/Resources/PolicyResourceTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Tests\Resources;
+
+use App\Http\Resources\PolicyResource;
+use App\Policy;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class PolicyResourceTest extends TestCase {
+    use RefreshDatabase;
+
+    public function testActiveFrom(): void {
+        $policy = Policy::create([
+            'policy_type' => 'terms-of-use',
+            'content_vue_file' => 'terms-of-use/example.vue',
+            'active_from' => '2022-02-02',
+        ]);
+
+        $resource = new PolicyResource($policy);
+        $data = $resource->resolve();
+
+        $this->assertSame(
+            '2022-02-02',
+            data_get($data, 'metadata.active_from'),
+        );
+    }
+
+    public function testActiveFromNull(): void {
+        $policy = Policy::create([
+            'policy_type' => 'terms-of-use',
+            'content_vue_file' => 'terms-of-use/example.vue',
+        ]);
+
+        $resource = new PolicyResource($policy);
+        $data = $resource->resolve();
+
+        $this->assertSame(
+            null,
+            data_get($data, 'metadata.active_from'),
+        );
+    }
+
+    public function testActiveFromNullExplicit(): void {
+        $policy = Policy::create([
+            'policy_type' => 'terms-of-use',
+            'content_vue_file' => 'terms-of-use/example.vue',
+            'active_from' => null,
+        ]);
+
+        $resource = new PolicyResource($policy);
+        $data = $resource->resolve();
+
+        $this->assertSame(
+            null,
+            data_get($data, 'metadata.active_from'),
+        );
+    }
+}
