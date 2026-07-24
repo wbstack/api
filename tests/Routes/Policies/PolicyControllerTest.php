fbc21c05b657202a69dad41ac36e09ef9cfb53c3
dena <91744937+deer-wmde@users.noreply.github.com>
2026-07-24 13:06:36 +0200

    endpoints for upcoming, current and all policies of a type (#1214)
    adds routes 
    ```
    /v1/policies/{policy_type}/current
    /v1/policies/{policy_type}/upcoming
    /v1/policies/{policy_type}
    ```
    
    ---
    
    Two observations:
    
    **1. api route order matters**
    this one works:
    ```php
        $router->get('v1/policies/current', ['uses' => 'PoliciesController@getCurrentPolicies']);
        $router->get('v1/policies/{policy_type}', ['uses' => 'PoliciesController@getPoliciesByType']);
    ```
    
    this one doesnt: `v1/policies/{policy_type}` swallows the later ones:
    ```php
        $router->get('v1/policies/{policy_type}', ['uses' => 'PoliciesController@getPoliciesByType']);
        $router->get('v1/policies/current', ['uses' => 'PoliciesController@getCurrentPolicies']);
    ```
    
    **2. Explicit nulling of `active_from`**
    Due to our PolicyFactory implementation we *need* to set `active_from`
    to `null` if we want it to be not filled.
    this works:
    ```php
            Policy::factory()->create([
                'policy_type' => 'terms-of-use',
                'active_from' => null,
            ]);
    ```
    
    this fills with `'active_from' => now()`
    ```php
            Policy::factory()->create([
                'policy_type' => 'terms-of-use',
            ]);
    ```
    
    https://phabricator.wikimedia.org/T432714

--- tests/Routes/Policies/PolicyControllerTest.php
+++ tests/Routes/Policies/PolicyControllerTest.php
@@ -3,6 +3,7 @@
 namespace Http\Controllers;
 
 use App\Policy;
+use Carbon\CarbonImmutable;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Tests\TestCase;
 
@@ -45,5 +46,116 @@ class PolicyControllerTest extends TestCase {
     public function testMissingPolicyByTypeAndActiveFromReturns404(): void {
         $request = $this->getJson('v1/policies/hosting-policy/by_active_from/2026-07-01');
         $request->assertNotFound();
+    }
+
+    public function testUpcomingTermsOfUseMissing(): void {
+        Policy::query()->delete();
+
+        $request = $this->getJson('v1/policies/terms-of-use/upcoming');
+        $request->assertNotFound();
+    }
+
+    public function testUpcomingHostingPolicyMissing(): void {
+        Policy::query()->delete();
+
+        $request = $this->getJson('v1/policies/hosting-policy/upcoming');
+        $request->assertNotFound();
+    }
+
+    public function testUpcomingTermsOfUseMultiple(): void {
+        Policy::query()->delete();
+
+        $now = CarbonImmutable::now();
+
+        Policy::factory()->create([
+            'policy_type' => 'terms-of-use',
+            'active_from' => $now->addWeek(),
+        ]);
+
+        Policy::factory()->create([
+            'policy_type' => 'terms-of-use',
+            'active_from' => null,
+        ]);
+
+        $request = $this->getJson('v1/policies/terms-of-use/upcoming');
+        $request->assertServerError();
+    }
+
+    public function testUpcomingTermsOfUse(): void {
+        Policy::query()->delete();
+
+        $now = CarbonImmutable::now();
+
+        Policy::factory()->create([
+            'policy_type' => 'terms-of-use',
+            'active_from' => $now->addWeek(),
+            'content_vue_file' => 'terms-of-use/version-1.vue',
+        ]);
+
+        $request = $this->getJson('v1/policies/terms-of-use/upcoming');
+
+        $request->assertOk();
+        $request->assertJsonStructure([
+            'metadata' => [
+                'policy_id',
+                'active_from',
+                'content_vue_file',
+                'type',
+            ],
+        ]);
+
+        $request->assertJsonFragment([
+            'active_from' => $now->addWeek()->format('Y-m-d'),
+            'type' => 'terms-of-use',
+            'content_vue_file' => 'terms-of-use/version-1.vue',
+        ]);
+    }
+
+    public function testCurrentTermsOfUseMissing(): void {
+        Policy::query()->delete();
+
+        $request = $this->getJson('v1/policies/terms-of-use/current');
+        $request->assertNotFound();
+    }
+
+    public function testCurrentHostingPolicyMissing(): void {
+        Policy::query()->delete();
+
+        $request = $this->getJson('v1/policies/hosting-policy/current');
+        $request->assertNotFound();
+    }
+
+    public function testCurrentTermsOfUse() {
+        $now = CarbonImmutable::now();
+
+        Policy::factory()->create([
+            'policy_type' => 'terms-of-use',
+            'active_from' => $now->subMonth(),
+            'content_vue_file' => 'terms-of-use/version-1.vue',
+        ]);
+
+        $current = Policy::factory()->create([
+            'policy_type' => 'terms-of-use',
+            'active_from' => $now->subWeek(),
+            'content_vue_file' => 'terms-of-use/version-2.vue',
+        ]);
+
+        $request = $this->getJson('v1/policies/terms-of-use/current');
+
+        $request->assertOk();
+        $request->assertJsonStructure([
+            'metadata' => [
+                'policy_id',
+                'active_from',
+                'content_vue_file',
+                'type',
+            ],
+        ]);
+
+        $request->assertJsonFragment([
+            'active_from' => $current->active_from->format('Y-m-d'),
+            'type' => $current->policy_type,
+            'content_vue_file' => $current->content_vue_file,
+        ]);
     }
 }
