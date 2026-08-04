4e205a60a0f98bc6483cd88734aa48c7e3a89f48
Dat <dat.nguyen@wikimedia.de>
2026-08-04 14:20:19 +0200

    Add SendPolicyAnnouncementJob job class

--- app/Jobs/SendPolicyAnnouncementJob.php
+++ app/Jobs/SendPolicyAnnouncementJob.php
@@ -2,21 +2,14 @@
 
 namespace App\Jobs;
 
-use App\Policy;
-use App\PolicyAnnouncement;
-use App\PolicyAcceptance;
+use App\Notifications\PolicyAnnouncementNotification;
 use App\User;
+use Illuminate\Support\Facades\Notification;
 
 class SendPolicyAnnouncementJob extends Job {
-
     public function handle() {
-        $users = User::all();
+        $users = User::query()->whereNotNull('email')->get();
 
-        foreach ($users as $user) {
-            // Here you would send the announcement to the user
-            // you might dispatch a notification or an email
-            // This is just a placeholder for the actual sending logic
-            // Notification::send($user, new PolicyAnnouncementNotification($policyAnnouncement));
-        }
+        Notification::send($users, new PolicyAnnouncementNotification());
     }
 }
