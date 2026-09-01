7f2a37747215cf9a65cc6560497716a30e4805c2
Ollie <43674967+outdooracorn@users.noreply.github.com>
2026-09-01 13:44:38 +0100

    Add warning docblock to SendPolicyAnnouncementJob (#1242)
    Add a warning to try and prevent us from copying this pattern in the future.
    
    Bug: T432211

--- app/Jobs/SendPolicyAnnouncementJob.php
+++ app/Jobs/SendPolicyAnnouncementJob.php
@@ -6,6 +6,16 @@ use App\Notifications\PolicyAnnouncementNotification;
 use App\User;
 use Illuminate\Support\Facades\Notification;
 
+/**
+ * WARNING: This job is NOT idempotent. DO NOT RUN IT MULTIPLE TIMES.
+ * There is also no error handling or mechanism for recording which users have been sent an email.
+ *
+ * This has created an issue on production where a user with an empty string as an email address
+ * (due to a request to remove PII) caused Notification::send() to throw an error and the remaining
+ * emails to not be sent.
+ *
+ * DO NOT REPEAT THIS PATTERN FOR OTHER JOBS
+ */
 class SendPolicyAnnouncementJob extends Job {
     public function handle() {
         $users = User::query()->whereNotNull('email')->get();
