0dbf83f8997f876e6b231647bbeb0b2385a71f71
Perside Rosalie <perside.rosalie@wikimedia.de>
2026-09-02 15:39:16 +0200

    Make sendPolicyAnnouncementJob more robust (#1243)
    This adds additional try-catch logic to make running this more robust.
    
    It specifically adds a test for a real error we saw caused by attempting
    to email
    a malformed email address.
    
    It does not record to the DB successful emails but it does add log
    output that
    will be persisted in GCE for longer than the mailgun logs.
    
    Bug: T432211
    Co-authored-by: Thomas Arrow <thomas.arrow@wikimedia.de>

--- app/Jobs/SendPolicyAnnouncementJob.php
+++ app/Jobs/SendPolicyAnnouncementJob.php
@@ -4,22 +4,30 @@ namespace App\Jobs;
 
 use App\Notifications\PolicyAnnouncementNotification;
 use App\User;
-use Illuminate\Support\Facades\Notification;
+use Illuminate\Support\Facades\Log;
 
 /**
  * WARNING: This job is NOT idempotent. DO NOT RUN IT MULTIPLE TIMES.
  * There is also no error handling or mechanism for recording which users have been sent an email.
  *
- * This has created an issue on production where a user with an empty string as an email address
+ * This has created an issue on production where a user with a weird string as an email address
  * (due to a request to remove PII) caused Notification::send() to throw an error and the remaining
  * emails to not be sent.
  *
  * DO NOT REPEAT THIS PATTERN FOR OTHER JOBS
  */
 class SendPolicyAnnouncementJob extends Job {
-    public function handle() {
+    public function handle(): void {
         $users = User::query()->whereNotNull('email')->get();
 
-        Notification::send($users, new PolicyAnnouncementNotification());
+        $users->each(function (User $user) {
+            try {
+                $user->notify(new PolicyAnnouncementNotification());
+
+                Log::info('PolicyAnnouncementNotification sent successfully', [$user->id, $user->email]);
+            } catch (\Exception $exception) {
+                Log::error($exception->getMessage(), [$user->id, $user->email]);
+            }
+        });
     }
 }
--- tests/Jobs/SendPolicyAnnouncementJobTest.php
+++ tests/Jobs/SendPolicyAnnouncementJobTest.php
@@ -7,6 +7,7 @@ use App\Notifications\PolicyAnnouncementNotification;
 use App\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Notification;
+use Symfony\Component\Mime\Exception\RfcComplianceException;
 use Tests\TestCase;
 
 class SendPolicyAnnouncementJobTest extends TestCase {
@@ -25,5 +26,25 @@ class SendPolicyAnnouncementJobTest extends TestCase {
         $job->handle();
 
         Notification::assertSentTo($users, PolicyAnnouncementNotification::class);
+    }
+
+    public function testItNotifiedAllUsersEvenIfMailerThrowsRfcComplianceException() {
+        // This test specifically simualtes the situation we saw in T432211#12270805
+        Notification::shouldReceive('send')
+            ->once()
+            ->andThrow(new RfcComplianceException());
+        Notification::shouldReceive('send')
+            ->atLeast()
+            ->times(3);
+
+        User::factory()->createMany([
+            ['email' => 'asdfghjklertyuiopcvbnm'],
+            ['email' => 'user1@email.com'],
+            ['email' => 'user2@email.com'],
+            ['email' => ''],
+            ['email' => 'user5@email.com'],
+        ]);
+        $job = new SendPolicyAnnouncementJob();
+        $job->handle();
     }
 }
