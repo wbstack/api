81b71869b7c0f20397469dea4335843654f8b1ee
Ollie <oliver.hyde@wikimedia.de>
2026-09-11 17:14:06 +0100

    Add migration, models, factories, and tests for review submissions
    Bug: T433682

---
+++ database/factories/ReviewSubmissionActionFactory.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Database\Factories;
+
+use App\Enums\ReviewSubmissionActionType;
+use App\Enums\UserRole;
+use App\ReviewSubmission;
+use App\ReviewSubmissionAction;
+use App\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<ReviewSubmissionAction>
+ */
+class ReviewSubmissionActionFactory extends Factory {
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array {
+        return [
+            'review_submission_id' => ReviewSubmission::factory(),
+            // TODO: this should be the same user as one of the ReviewSubmission::wiki->wikiManagersWithEmail
+            'actor_user_id' => User::factory(),
+            'actor_user_role' => UserRole::WIKI_MANAGER,
+            'type' => ReviewSubmissionActionType::SUBMITTED,
+        ];
+    }
+
+    public function forActor(User $actor): static {
+        return $this->for($actor, 'actor');
+    }
+}
